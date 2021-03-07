5bf56dd1c175920fd7413be5868b34aa767cafe2
Julián David <julianfullstackdev@gmail.com>
2021-03-07 18:17:02 -0500

    add title

--- components/web/layout/head.tsx
+++ components/web/layout/head.tsx
@@ -3,12 +3,13 @@ import Head from "next/head";
 const HeadContainer = () => {
   return (
     <Head>
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
+        <title>Tinta Love - Amamos el tatuaje</title>
+        <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+        />
     </Head>
   );
 };
