648d7197e38a20212e78b18028c6571db91ceee1
Julian David <julianfullstackdev@gmail.com>
2021-03-14 19:09:29 -0500

    better icons

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -4,74 +4,35 @@ const HeadContainer = () => {
   return (
     <Head>
       <title>Tinta Love - Amamos el tatuaje</title>
+
       <link
         rel="apple-touch-icon"
-        sizes="57x57"
-        href="/favicon/apple-icon-57x57.png"
+        sizes="512x512"
+        href="/favicon/android/android-launchericon-512-512.png"
       />
-      <link
-        rel="apple-touch-icon"
-        sizes="60x60"
-        href="/favicon/apple-icon-60x60.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="72x72"
-        href="/favicon/apple-icon-72x72.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="76x76"
-        href="/favicon/apple-icon-76x76.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="114x114"
-        href="/favicon/apple-icon-114x114.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="120x120"
-        href="/favicon/apple-icon-120x120.png"
-      />
+
       <link
         rel="apple-touch-icon"
         sizes="144x144"
-        href="/favicon/apple-icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="152x152"
-        href="/favicon/apple-icon-152x152.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-icon-180x180.png"
+        href="/favicon/android/android-launchericon-144-144.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/favicon/android-icon-192x192.png"
+        href="/favicon/android/android-launchericon-192-192.png"
       />
       <link
         rel="icon"
         type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        sizes="48x48"
+        href="/favicon/android/android-launchericon-48-48.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="96x96"
-        href="/favicon/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon/android/android-launchericon-96-96.png"
       />
       <link rel="manifest" href="/manifest.json" />
       <meta name="msapplication-TileColor" content="#ffffff" />
