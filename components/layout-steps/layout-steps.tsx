76973653155743bfda8e7490a879961f08fde87f
Julián David <julianfullstackdev@gmail.com>
2021-04-10 20:03:25 -0500

    better responsive for tablet screens

--- components/layout-steps/layout-steps.tsx
+++ components/layout-steps/layout-steps.tsx
@@ -7,7 +7,7 @@ import { RiArrowGoBackFill } from 'react-icons/ri'
 const LayoutSteps = ({ children }) => {
   return (
     <div className="grid grid-cols-10 h-screen">
-      <div className="col-span-2 bg-dark-800 pl-12 pt-8 row-span-full">
+      <div className="col-span-3 2xl:col-span-2 bg-dark-800 pl-10 2xl:pl-12 pt-8 row-span-full">
         <div className="w-52 relative h-11 mb-20">
           <Link href="/">
             <a>
@@ -23,8 +23,8 @@ const LayoutSteps = ({ children }) => {
         </div>
         <SideMenu />
       </div>
-      <div className="col-span-8 bg-dark-500 text-white">
-        <header className="flex justify-between pt-6 pl-20 pr-10">
+      <div className="col-span-7 2xl:col-span-8 pl-14 2xl:pl-20 bg-dark-500 text-white">
+        <header className="flex justify-between pt-6  pr-10">
           <Link href="/">
             <a className="flex items-center">
               <RiArrowGoBackFill className="mr-5" /> Volver al inicio
@@ -34,7 +34,7 @@ const LayoutSteps = ({ children }) => {
             <StepNav />
           </div>
         </header>
-        <main className="pl-20">{children}</main>
+        <main>{children}</main>
       </div>
     </div>
   )
