b7cfa749d907dd7d570a2442cb41ec4a0e405904
Julián David <julianfullstackdev@gmail.com>
2021-04-12 12:29:27 -0500

    new tooltip implemented

--- components/artist-account/main-info-edit/main-info-available.tsx
+++ components/artist-account/main-info-edit/main-info-available.tsx
@@ -1,5 +1,3 @@
-import Popup from 'reactjs-popup'
-import 'reactjs-popup/dist/index.css'
 import { FiAlertCircle, FiCheckCircle, FiHelpCircle } from 'react-icons/fi'
 
 const MainInfoAvailable = ({ validUserName, availableUserName }) => {
@@ -20,40 +18,31 @@ const MainInfoAvailable = ({ validUserName, availableUserName }) => {
                 No disponible
                 <FiAlertCircle className="ml-1 text-2xl text-red-500" />
               </span>
-              <Popup
-                trigger={
-                  <span>
-                    <FiHelpCircle className="text-xl ml-3 cursor-help" />
-                  </span>
-                }
-                on={['hover', 'focus']}
-                position="right center"
+
+              <span
+                aria-label="Intenta con otro usuario en el cuadro de abajo."
+                data-microtip-position="bottom"
+                data-microtip-size="medium"
+                role="tooltip"
               >
-                <div className="text-sm">
-                  Intenta con otro usuario en el cuadro de abajo.
-                </div>
-              </Popup>
+                <FiHelpCircle className="text-xl ml-3 cursor-help" />
+              </span>
             </div>
           )}
         </div>
       ) : (
         <div className="flex items-center">
           <span>Formato inválido</span>
-          <Popup
-            trigger={
-              <span>
-                <FiHelpCircle className="text-xl ml-3 cursor-help" />
-              </span>
-            }
-            on={['hover', 'focus']}
-            position="right center"
-            keepTooltipInside=".tooltipBox"
+
+          <span
+            aria-label="Intenta con otro nombre de usuario, puedes usar letras, números.
+              También _ y . pero no seguidos o al final del usuario"
+            data-microtip-position="bottom"
+            data-microtip-size="medium"
+            role="tooltip"
           >
-            <div className="text-sm">
-              Intenta con otro nombre de usuario, puedes usar letras, números.
-              También _ y . pero no seguidos o al final del usuario
-            </div>
-          </Popup>
+            <FiHelpCircle className="text-xl ml-3 cursor-help" />
+          </span>
         </div>
       )}
     </>
