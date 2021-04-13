6bd9b104ce82153815ff805d5874b9c7006d68b9
Julián David <julianfullstackdev@gmail.com>
2021-04-13 12:00:19 -0500

    new city library ready

--- components/artist-account/main-info-edit/main-info-city.tsx
+++ components/artist-account/main-info-edit/main-info-city.tsx
@@ -4,8 +4,14 @@ import usePlacesAutocomplete, {
 } from 'use-places-autocomplete'
 import useOnclickOutside from 'react-cool-onclickoutside'
 import useScript from 'hooks/use-script'
+import { useState } from 'react'
+import { geohashForLocation } from 'geofire-common'
 
-const MainInfoCity = () => {
+const MainInfoCity = ({ defaultValue, setPlaceInfo }) => {
+  const [placeholder, setPlaceholder] = useState('')
+
+  console.log(defaultValue, 'valor por defecto')
+
   const {
     ready,
     value,
@@ -19,38 +25,65 @@ const MainInfoCity = () => {
     },
     debounce: 300,
     callbackName: 'initMap',
+    defaultValue: defaultValue,
   })
 
   const statusMap = useScript(
     'https://maps.googleapis.com/maps/api/js?key=AIzaSyA5drETj_sJmO1kGEDEb7tXWzwJb05ipCY&libraries=places&callback=initMap'
   )
 
-  // const ref = useOnclickOutside(() => {
-  //   // When user clicks outside of the component, we can dismiss
-  //   // the searched suggestions by calling this method
-  //   clearSuggestions()
-  // })
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions()
+  })
 
   const handleInput = (e) => {
     // Update the keyword of the input element
     setValue(e.target.value)
   }
 
-  const handleSelect = ({ description }) => () => {
-    // When user selects a place, we can replace the keyword without request data from API
-    // by setting the second parameter to "false"
+  const handleSelect = ({ description }) => async () => {
+    //pedir explicación aquí a lucho
+    // // When user selects a place, we can replace the keyword without request data from API
+    // // by setting the second parameter to "false"
     setValue(description, false)
     clearSuggestions()
+    setPlaceholder(description)
 
     // Get latitude and longitude via utility functions
-    getGeocode({ address: description })
-      .then((results) => getLatLng(results[0]))
-      .then(({ lat, lng }) => {
-        console.log('📍 Coordinates: ', { lat, lng })
-      })
-      .catch((error) => {
-        console.log('😱 Error: ', error)
-      })
+    const results = await getGeocode({ address: description })
+
+    const { lat, lng }: any = await getLatLng(results[0])
+
+    console.log(lat, lng, 'los lat y long')
+
+    const cityHash = geohashForLocation([lat, lng])
+
+    const fullAddress = results[0].formatted_address.split(',')
+    const city_name = fullAddress[0]
+    const province = fullAddress[1].trim() || ''
+    const country = (fullAddress[2] && fullAddress[2].trim()) || 'Colombia'
+
+    setPlaceInfo({
+      place_id: results[0].place_id,
+      formatted_address: results[0].formatted_address,
+      city_name,
+      city_hash: cityHash,
+      province,
+      country,
+    })
+
+    // getGeocode()
+    //   .then((results) => getLatLng(results[0]))
+    //   .then(({ lat, lng }) => {
+
+    //     console.log('📍 Coordinates: ', { lat, lng })
+
+    //   })
+    //   .catch((error) => {
+    //     console.log('😱 Error: ', error)
+    //   })
   }
 
   const renderSuggestions = () =>
@@ -72,15 +105,21 @@ const MainInfoCity = () => {
     })
 
   return (
-    // <div ref={ref}>
-    <div className="relative">
+    <div className="relative" ref={ref}>
       <input
         value={value}
         onChange={handleInput}
         disabled={!ready}
-        placeholder="Busca tu ciudad"
+        placeholder={
+          placeholder
+            ? placeholder
+            : defaultValue
+            ? defaultValue
+            : 'Busca tu ciudad'
+        }
         className="input-primary w-full"
         spellCheck="false"
+        onFocus={() => setValue('')}
       />
       {/* We can use the "status" to decide whether we should display the dropdown or not */}
       {status === 'OK' && (
