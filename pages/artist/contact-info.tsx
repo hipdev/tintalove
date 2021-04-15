9ef89733c054de938bf30bdedd7f2777fe5e3fbb
Julián David <julianfullstackdev@gmail.com>
2021-04-14 22:33:23 -0500

    started new design contact info

--- pages/artist/contact-info.tsx
+++ pages/artist/contact-info.tsx
@@ -1,10 +1,26 @@
 import ContactInfo from 'components/artist-account/contact-info'
+import IsAuth from 'components/isAuth'
+import LayoutSteps from 'components/layout-steps/layout-steps'
 import Layout from 'components/layout/layout'
+import useUser from 'hooks/use-user'
 
-export default function MainInfoPage() {
+export default function ContactInfoPage() {
+  const { state } = useUser()
+  const isArtist = state?.user?.is_artist
+
   return (
-    <Layout>
-      <ContactInfo />
-    </Layout>
+    <>
+      {state?.user ? (
+        <LayoutSteps uid={state?.user?.uid}>
+          {state && state.user && (
+            <ContactInfo uid={state?.user?.uid || null} isArtist={isArtist} />
+          )}
+        </LayoutSteps>
+      ) : (
+        <LayoutSteps>
+          <IsAuth>Cargando...</IsAuth>
+        </LayoutSteps>
+      )}
+    </>
   )
 }
