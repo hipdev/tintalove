917629d80699b6e68fbc2e75ea3d0fb89d4f2a2a
Julián David <julianfullstackdev@gmail.com>
2021-04-16 20:08:00 -0500

    created imagekit api to generate token and add axios to work with fetch more easy

--- pages/api/imagekit/auth.ts
+++ pages/api/imagekit/auth.ts
@@ -7,15 +7,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (method === 'GET') {
     try {
       const imagekit = new ImageKit({
-        publicKey: 'CBvEZBsrd4kXEr/2BzHTqEmxbkQ=',
-        privateKey: 'lVGrq3bYDrRQBzzm0xEYOkVdGv0=',
-        urlEndpoint: 'https://ik.imagekit.io/fdvb7bhwbsbnxqnpunlj',
+        publicKey: 'public_EUtZgctR8vm6PmW9JTeqTLQI4AM=',
+        privateKey: 'private_tD7OrL3QMs7c3jCKdGIddrBFJkE=',
+        urlEndpoint: 'https://ik.imagekit.io/Home/',
       })
       const token = uuidv4()
       const expire = Date.now() / 1000 + 2400
-      const authenticationParameters = imagekit.getAuthenticationParameters(token, expire)
+      const authenticationParameters = imagekit.getAuthenticationParameters(
+        token,
+        expire
+      )
 
-      if (!authenticationParameters.signature) throw new Error('parameters not found')
+      if (!authenticationParameters.signature)
+        throw new Error('parameters not found')
       res.status(200).send(authenticationParameters)
     } catch (error) {
       res.status(400).send(error.message)
