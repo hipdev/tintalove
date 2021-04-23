935de2d5a0c063f4c1e5ce1c611b719f3ede26f4
Julián David <julianfullstackdev@gmail.com>
2021-04-23 12:48:24 -0500

    started studio account

---
+++ pages/studio-account/general.tsx
@@ -0,0 +1,28 @@
+import MainInfo from 'components/artist-account/main-info'
+import MainInfoEdit from 'components/artist-account/main-info-edit/main-info-edit'
+import IsAuth from 'components/isAuth'
+import LayoutSteps from 'components/layout-steps/layout-steps'
+import useUser from 'hooks/use-user'
+
+export default function MainInfoPage() {
+  const { state } = useUser()
+  const isArtist = state?.user?.is_artist
+
+  return (
+    <>
+      {state?.user ? (
+        <LayoutSteps uid={state?.user?.uid} userState={state?.user || null}>
+          {state && state.user && isArtist ? (
+            <MainInfoEdit uid={state?.user?.uid || null} />
+          ) : (
+            <MainInfo uid={state?.user?.uid || null} />
+          )}
+        </LayoutSteps>
+      ) : (
+        <LayoutSteps>
+          <IsAuth>Cargando...</IsAuth>
+        </LayoutSteps>
+      )}
+    </>
+  )
+}
