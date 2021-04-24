a115e47cf7c1197bb97270e8ea3cfcd58cc01d3b
Julián David <julianfullstackdev@gmail.com>
2021-04-24 11:04:17 -0500

    advance

--- components/studio-account/general-info-edit/general-info-edit.tsx
+++ components/studio-account/general-info-edit/general-info-edit.tsx
@@ -1,5 +1,5 @@
 import useArtist from 'hooks/use-artist'
-import MainInfoForm from './main-info-form'
+import MainInfoForm from './general-info-form'
 
 const MainInfoEdit = ({ uid }) => {
   const { artist } = useArtist(uid)
@@ -7,7 +7,7 @@ const MainInfoEdit = ({ uid }) => {
   return (
     <div className="w-4/5 mt-10">
       <h1 className="text-white text-xl sm:text-2xl font-bold  sm:text-left tracking-wide mb-2">
-        Información personal
+        Información general
       </h1>
       <p className="text-white mb-5 sm:mb-6 lg:mb-8">
         Gracias por ser parte de la familia Tinta Love, cuando llenes todos los
