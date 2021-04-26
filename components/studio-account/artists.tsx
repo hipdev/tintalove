181e4ac5613946c2b8652755a5279100039f8f8b
Julián David <julianfullstackdev@gmail.com>
2021-04-25 20:30:57 -0500

    component for send emails

--- components/studio-account/artists.tsx
+++ components/studio-account/artists.tsx
@@ -7,6 +7,7 @@ import toast, { Toaster } from 'react-hot-toast'
 import { updateArtistWorkingInfo } from 'lib/db'
 import { useRouter } from 'next/router'
 import useArtist from 'hooks/use-artist'
+import ArtistsSendEmail from './artists-send-email'
 
 const options = tattooStyles.map((style) => {
   return { value: style, label: style }
@@ -94,36 +95,7 @@ const Artists = ({ uid, isArtist }) => {
       </h1>
 
       <form className="mt-10" onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="text-xl font-semibold">Invitar un artista</h2>
-        <p className="mb-5 text-sm">
-          Escribe el nombre y correo, le enviaremos una invitación para que
-          pueda ser parte de tu estudio
-        </p>
-        <div className="grid grid-cols-10 gap-6 mb-5">
-          <div className="col-span-4">
-            <label className="text-sm mb-3 tracking-wide">
-              <span className="mb-3 block">NOMBRE</span>
-              <input
-                type="text"
-                className="input-primary w-full"
-                placeholder="Nombre del artista"
-              />
-            </label>
-          </div>
-          <div className="col-span-4">
-            <label className="text-sm mb-3 tracking-wide">
-              <span className="mb-3 block">EMAIL</span>
-              <input
-                type="text"
-                className="input-primary w-full"
-                placeholder="Email del artista"
-              />
-            </label>
-          </div>
-          <div className="flex justify-end">
-            <button>Enviar</button>
-          </div>
-        </div>
+        <ArtistsSendEmail />
 
         <div>
           <h2 className="text-xl font-semibold"> Artistas actuales</h2>
