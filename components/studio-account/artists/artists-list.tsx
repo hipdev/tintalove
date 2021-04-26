0166d9e90bd34617cb4d655ef3512eb9f5d52ca2
Julián David <julianfullstackdev@gmail.com>
2021-04-26 10:09:39 -0500

    better design for list of artists

--- components/studio-account/artists/artists-list.tsx
+++ components/studio-account/artists/artists-list.tsx
@@ -1,16 +1,18 @@
+import Link from 'next/link'
+import { AiOutlineDelete } from 'react-icons/ai'
 import { MdCheckCircle, MdChevronRight, MdMail } from 'react-icons/md'
 
 const applications = [
   {
     applicant: {
-      name: 'Ricardo Cooper',
-      email: 'ricardo.cooper@example.com',
+      name: 'Dann Coly',
+      email: 'dancoly@gmail.com',
       imageUrl:
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
+    dateFull: 'Enero 7 de 2021',
+    stage: 'Es parte del estudio',
     href: '#',
   },
   {
@@ -22,7 +24,7 @@ const applications = [
     },
     date: '2020-01-07',
     dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
+    stage: 'Es parte del estudio',
     href: '#',
   },
   {
@@ -34,7 +36,7 @@ const applications = [
     },
     date: '2020-01-07',
     dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
+    stage: 'Es parte del estudio',
     href: '#',
   },
 ]
@@ -45,59 +47,66 @@ const ArtistsAccountList = () => {
       <div className="bg-dark-800 shadow overflow-hidden sm:rounded-sm mb-10">
         <ul className="divide-y divide-gray-200">
           {applications.map((application) => (
-            <li key={application.applicant.email}>
-              <a href={application.href} className="block hover:bg-gray-50">
-                <div className="flex items-center px-4 py-4 sm:px-6">
-                  <div className="min-w-0 flex-1 flex items-center">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-12 w-12 rounded-full"
-                        src={application.applicant.imageUrl}
-                        alt=""
-                      />
-                    </div>
-                    <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                      <div>
-                        <p className="text-sm font-medium text-indigo-600 truncate">
-                          {application.applicant.name}
-                        </p>
-                        <p className="mt-2 flex items-center text-sm text-gray-500">
-                          <MdMail
-                            className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                            aria-hidden="true"
-                          />
-                          <span className="truncate">
-                            {application.applicant.email}
-                          </span>
-                        </p>
-                      </div>
-                      <div className="hidden md:block">
-                        <div>
-                          <p className="text-sm text-gray-900">
-                            Applied on{' '}
-                            <time dateTime={application.date}>
-                              {application.dateFull}
-                            </time>
-                          </p>
-                          <p className="mt-2 flex items-center text-sm text-gray-500">
-                            <MdCheckCircle
-                              className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
-                              aria-hidden="true"
-                            />
-                            {application.stage}
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div>
-                    <MdChevronRight
-                      className="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
+            <li
+              key={application.applicant.email}
+              className="block hover:bg-gray-900"
+            >
+              <div className="flex items-center px-4 py-4 sm:px-6">
+                <div className="min-w-0 flex-1 flex items-center">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-12 w-12 rounded-full"
+                      src={application.applicant.imageUrl}
+                      alt=""
                     />
                   </div>
+                  <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
+                    <div>
+                      <a
+                        href="#"
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-sm font-medium text-primary truncate"
+                      >
+                        {application.applicant.name}
+                      </a>
+
+                      <p className="mt-2 flex items-center text-sm text-gray-500">
+                        <MdMail
+                          className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                        <span className="truncate">
+                          {application.applicant.email}
+                        </span>
+                      </p>
+                    </div>
+                    <div className="hidden md:block">
+                      <div>
+                        <p className="text-sm text-gray-400">
+                          Aplico en{' '}
+                          <time dateTime={application.date}>
+                            {application.dateFull}
+                          </time>
+                        </p>
+                        <p className="mt-2 flex items-center text-sm text-gray-500">
+                          <MdCheckCircle
+                            className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
+                            aria-hidden="true"
+                          />
+                          {application.stage}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </a>
+                <div>
+                  <AiOutlineDelete
+                    className="h-5 w-5 text-red-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </div>
             </li>
           ))}
         </ul>
