638da0d8be29a8293b6159690ac835130bcef585
Julián David <julianfullstackdev@gmail.com>
2021-04-27 16:32:53 -0500

    validate email

--- pages/studio-account/artists.tsx
+++ pages/studio-account/artists.tsx
@@ -1,11 +1,14 @@
 import IsAuth from 'components/isAuth'
 import LayoutStepsStudio from 'components/layout-steps/layout-steps-studio'
 import Artists from 'components/studio-account/artists/artists'
+import useStudio from 'hooks/use-studio'
 
 import useUser from 'hooks/use-user'
+import { UserState } from 'types/user'
 
-export default function StudioArtists() {
-  const { state } = useUser()
+export default function StudioArtists({ studioId }) {
+  const { state }: { state: { user: UserState } } = useUser()
+
   const isArtist = state?.user?.is_artist
 
   return (
@@ -16,7 +19,11 @@ export default function StudioArtists() {
           userState={state?.user || null}
         >
           {state && state.user && (
-            <Artists uid={state?.user?.uid || null} isArtist={isArtist} />
+            <Artists
+              uid={state?.user?.uid || null}
+              hasStudio={state?.user?.has_studio}
+              studioId={state?.user?.studio_id}
+            />
           )}
         </LayoutStepsStudio>
       ) : (
