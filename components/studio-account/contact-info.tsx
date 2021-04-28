1d5529cf4041f3495313295b0348299666075d45
Julián David <julianfullstackdev@gmail.com>
2021-04-27 21:52:44 -0500

    step 3 creating studio ready

---
+++ components/studio-account/contact-info.tsx
@@ -0,0 +1,253 @@
+import useStudio from 'hooks/use-studio'
+import { updateStudioContactInfo } from 'lib/db'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import toast, { Toaster } from 'react-hot-toast'
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+const ContactInfoStudio = ({ studioId, hasStudio }) => {
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const { studio } = useStudio(studioId)
+  const router = useRouter()
+
+  const {
+    register,
+    setValue,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      contact_way: '',
+      phone: '',
+      instagram: '',
+      facebook: '',
+      twitter: '',
+    },
+  })
+
+  useEffect(() => {
+    if (studio) {
+      let styles = []
+      if (studio.styles) {
+        styles = studio.styles.map((style) => ({
+          label: style,
+          value: style,
+        }))
+      }
+
+      setValue('contact_way', studio.contact_way)
+      setValue('phone', studio.phone)
+      setValue('instagram', studio.instagram)
+      setValue('facebook', studio.facebook)
+      setValue('twitter', studio.twitter)
+    }
+  }, [studio])
+
+  useEffect(() => {
+    if (success) {
+      const timer = setTimeout(() => router.push('/artist/pictures-info'), 1000)
+      return () => clearTimeout(timer)
+    }
+  }, [success])
+
+  const watchContactWay = watch('contact_way')
+
+  const onSubmit = (data) => {
+    console.log(data, 'data form')
+    setLoading(true)
+
+    toast.promise(updateStudioContactInfo(studioId, data, true), {
+      loading: 'Actualizando...',
+      success: () => {
+        setLoading(false)
+        setSuccess(true)
+
+        return 'Artista actualizado 😉'
+      },
+      error: (err) => {
+        setLoading(false)
+        return `${err.toString()}`
+      },
+    })
+
+    setLoading(false)
+  }
+
+  return (
+    <div className="w-4/5 mt-10">
+      <Toaster
+        toastOptions={{
+          className: 'bg-red-600',
+          style: {
+            background: '#ef3e30',
+            border: 'none',
+            borderRadius: '3px',
+            color: '#fff',
+          },
+          duration: 5000,
+        }}
+        position="bottom-right"
+      />
+      <h1 className="text-xl sm:text-2xl font-bold  sm:text-left tracking-wide mb-2">
+        Información de Contacto
+      </h1>
+
+      <form className="mt-10" onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid grid-cols-6 gap-6">
+          <div className="col-span-6 lg:col-span-4 xl:col-span-3">
+            <label
+              htmlFor=""
+              className="block text-white text-sm  mb-3 tracking-wide"
+            >
+              <span className="mb-3 block">
+                POR DÓNDE QUIERES QUE TE CONTACTEN
+              </span>
+
+              <select
+                className="w-full input-primary form-select p-3 text-sm bg-dark-500 focus:ring-dark-800 focus:border-dark-800"
+                {...register('contact_way', { required: true })}
+              >
+                <option value="">Selecciona por favor</option>
+                <option value="direct-call">Llamada directa</option>
+                <option value="whatsapp">WhatsApp</option>
+                <option value="telegram">Telegram</option>
+                <option value="chat-instagram">Chat de Instagram</option>
+              </select>
+              {errors.contact_way && (
+                <p className="mt-1">Esta campo es requerido</p>
+              )}
+            </label>
+          </div>
+          <div className="col-span-6 lg:col-span-4 xl:col-span-3">
+            <label
+              htmlFor=""
+              className="block text-white text-sm mb-3 tracking-wide"
+            >
+              <span className="mb-3 block">NÚMERO</span>
+
+              <Controller
+                rules={{ required: true }}
+                control={control}
+                name="phone"
+                render={({ field }) => (
+                  <PhoneInput
+                    country={'co'}
+                    onlyCountries={[
+                      'co',
+                      'es',
+                      'ar',
+                      'it',
+                      'ec',
+                      'br',
+                      'pe',
+                      'us',
+                      'ur',
+                      'mx',
+                    ]}
+                    containerClass="input-primary p-1"
+                    searchStyle={{ background: 'red' }}
+                    inputStyle={{
+                      background: '#111319',
+                      border: 'none',
+                      color: '#fff',
+                    }}
+                    buttonStyle={{
+                      background: '#111319',
+                      border: 'none',
+                    }}
+                    dropdownStyle={{
+                      fontFamily: 'Inter',
+                      background: '#080a12',
+                      color: '#e2e2e2',
+                    }}
+                    {...field}
+                  />
+                )}
+              />
+              {errors.phone && <p className="mt-1">Esta campo es requerido</p>}
+            </label>
+          </div>
+          <div className="col-span-6 lg:col-span-4 xl:col-span-3">
+            <label
+              htmlFor=""
+              className="block text-white text-sm  mb-3 tracking-wide"
+            >
+              <span className="mb-3 block">INSTAGRAM</span>
+
+              <input
+                type="text"
+                placeholder="Pega la URL de tu perfil"
+                className="w-full input-primary"
+                {...register('instagram', {
+                  required: watchContactWay == 'chat-instagram' ? true : false,
+                })}
+              />
+              {errors.phone && <p className="mt-1">Esta campo es requerido</p>}
+            </label>
+          </div>
+          <div className="col-span-6 lg:col-span-4 xl:col-span-3">
+            <label
+              htmlFor=""
+              className="block text-white text-sm  mb-3 tracking-wide"
+            >
+              <span className="mb-3 block">FACEBOOK</span>
+
+              <input
+                type="text"
+                placeholder="Pega la URL de tu perfil"
+                className="w-full input-primary"
+                {...register('facebook')}
+              />
+            </label>
+          </div>
+          <div className="col-span-6 lg:col-span-4 xl:col-span-3">
+            <label
+              htmlFor=""
+              className="block text-white text-sm  mb-3 tracking-wide"
+            >
+              <span className="mb-3 block">TWITTER</span>
+
+              <input
+                type="text"
+                placeholder="Pega la URL de tu perfil"
+                className="w-full input-primary"
+                {...register('twitter')}
+              />
+            </label>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          {!hasStudio && (
+            <p className="text-white">
+              Primero debes guardar el Paso 1, Información Personal.
+            </p>
+          )}
+          {hasStudio ? (
+            <button
+              type="submit"
+              className="block  btn-primary py-3 px-5"
+              disabled={loading}
+            >
+              Siguiente
+            </button>
+          ) : (
+            <Link href="/artist/main-info">
+              <button className="block   btn-primary py-3 px-5">
+                Ir al paso 1
+              </button>
+            </Link>
+          )}
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ContactInfoStudio
---
+++ pages/studio-account/contact-info.tsx
@@ -0,0 +1,32 @@
+import IsAuth from 'components/isAuth'
+import LayoutStepsStudio from 'components/layout-steps/layout-steps-studio'
+import ContactInfoStudio from 'components/studio-account/contact-info'
+
+import useUser from 'hooks/use-user'
+
+export default function ContactInfoPageStudio() {
+  const { state } = useUser()
+  const hasStudio = state?.user?.has_studio
+
+  return (
+    <>
+      {state?.user ? (
+        <LayoutStepsStudio
+          uid={state?.user?.uid}
+          userState={state?.user || null}
+        >
+          {state && state.user && (
+            <ContactInfoStudio
+              studioId={state?.user?.studio_id || null}
+              hasStudio={hasStudio}
+            />
+          )}
+        </LayoutStepsStudio>
+      ) : (
+        <LayoutStepsStudio>
+          <IsAuth>Cargando...</IsAuth>
+        </LayoutStepsStudio>
+      )}
+    </>
+  )
+}
