db7e271da1cdbbbb1155f509ff6b70593ccb7ab4
Julián David <julianfullstackdev@gmail.com>
2021-05-02 19:26:11 -0500

    start firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions'
+
+import algoliasearch from 'algoliasearch'
+
+const APP_ID = functions.config().algolia.app
+const ADMIN_KEY = functions.config().algolia.key
+
+const client = algoliasearch(APP_ID, ADMIN_KEY)
+
+const index = client.initIndex('tintalove_dev')
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.addToIndex = functions.firestore
+  .document('artists/{artistId}')
+  .onCreate((snapshot) => {
+    const data = snapshot.data()
+    const objectID = snapshot.id
+  })
