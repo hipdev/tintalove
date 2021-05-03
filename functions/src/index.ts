08b6b3b8899d705772b21fbbe22e648cb9cf58b1
Julián David <julianfullstackdev@gmail.com>
2021-05-02 22:55:15 -0500

    ready to make the first functions work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,13 @@
 import * as functions from 'firebase-functions'
 
-import algoliasearch from 'algoliasearch'
+import algoliasearch, { SearchIndex } from 'algoliasearch'
 
 const APP_ID = functions.config().algolia.app
 const ADMIN_KEY = functions.config().algolia.key
 
 const client = algoliasearch(APP_ID, ADMIN_KEY)
 
-const index = client.initIndex('tintalove_dev')
+const index: any = client.initIndex('tintalove_dev')
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -17,7 +17,22 @@ exports.addToIndex = functions.firestore
   .onCreate((snapshot) => {
     const data = snapshot.data()
     const objectID = snapshot.id
+
+    return index.addObject({ ...data, objectID })
   })
+
+const updateIndex = functions.firestore
+  .document('artists/{artistId}')
+  .onUpdate((change) => {
+    const newData = change.after.data()
+    const objectID = change.after.id
+
+    return index.saveObject({ ...newData, objectID })
+  })
+
+const deleteFromIndex = functions.firestore
+  .document('artists/{artistId}')
+  .onDelete((snapshot) => index.deleteObject(snapshot.id))
 
 const addToIndex2 = functions.firestore
   .document('users/{artistId}')
@@ -26,4 +41,4 @@ const addToIndex2 = functions.firestore
     const objectID = snapshot.id
   })
 
-export { addToIndex2 }
+export { addToIndex2, updateIndex, deleteFromIndex }
