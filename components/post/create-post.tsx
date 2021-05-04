627dc1a529fa593092fb7127a18c6b9ffd3ee780
Julián David <julianfullstackdev@gmail.com>
2021-05-04 10:08:13 -0500

    design create post

--- components/post/create-post.tsx
+++ components/post/create-post.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import CreatePostPicture from './create-post-picture'
+import { IoMdTabletLandscape, IoMdTabletPortrait } from 'react-icons/io'
 
 const CreatePost = ({ uid }) => {
   const [description, setDescription] = useState('')
   const [styles, setStyles] = useState('')
+  const [withPicture, setWithPicture] = useState(false)
+  const [isPortrait, setIsPortrait] = useState(true)
 
   return (
-    <div className="bg-dark-800 pt-10 pb-48 h-3/6">
-      <div className="bg-dark-500 container mx-auto flex w-4/5 py-12 px-10  ">
-        <div className="w-2/3 pr-10">
-          <CreatePostPicture uid={uid} dataForm={{ description, styles }} />
+    <div className="bg-dark-800 pt-10 pb-48 h-3/6 h-auto xl:h-screen">
+      <div className="bg-dark-500 container mx-auto flex w-4/5 xl:w-3/5  py-12 px-10  ">
+        <div className="w-2/3  pr-10">
+          <CreatePostPicture
+            uid={uid}
+            dataForm={{ description, styles }}
+            setWithPicture={setWithPicture}
+          />
         </div>
 
         <div className="w-1/3 text-gray-300">
@@ -33,6 +40,35 @@ const CreatePost = ({ uid }) => {
               onChange={(e) => setStyles(e.target.value)}
             />
           </label>
+
+          {withPicture && (
+            <div className="mt-14 text-center">
+              <h2 className="font-semibold text-xl mb-5">
+                Orientación de la foto
+              </h2>
+              <div className="flex justify-around">
+                <div className={isPortrait ? 'text-primary' : ''}>
+                  <button
+                    className="outline-none focus:outline-none relative top-3 hover:text-primary"
+                    onClick={() => setIsPortrait(true)}
+                  >
+                    <IoMdTabletPortrait className="text-7xl" />
+                    <span className="mt-3 block">Retrato</span>
+                  </button>
+                </div>
+                <div className={!isPortrait ? 'text-primary' : ''}>
+                  <button
+                    className="hover:text-primary focus:outline-none"
+                    onClick={() => setIsPortrait(false)}
+                  >
+                    <IoMdTabletLandscape className="text-8xl" />
+
+                    <span>Paisaje</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
