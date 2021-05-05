cfd09212430209110644692d77a3d7194922f54f
Julián David <julianfullstackdev@gmail.com>
2021-05-04 22:59:30 -0500

    creating post ready ;)

--- lib/db.ts
+++ lib/db.ts
@@ -280,6 +280,7 @@ export async function updateArtistMainProfilePicture(
     throw new Error('No estas registrado como artista')
   }
 }
+
 export async function activateArtist(uid) {
   const artistRef = doc(collection(db, 'artists'), uid)
   const usersRef = doc(collection(db, 'users'), uid)
@@ -343,6 +344,32 @@ export async function updateArtistUsername(uid, oldUsername, newUsername) {
   await batch.commit()
 
   return true
+}
+
+export async function createArtistPost(uid, infoPicture, dataForm, artist) {
+  if (!dataForm?.description || dataForm?.styles.length < 0) {
+    throw new Error('Te faltan los campos del formulario')
+  }
+  if (!infoPicture) {
+    throw new Error('Te falta agregar la foto')
+  }
+  const styles = dataForm.styles.map((style) => style.value)
+
+  await addDoc(collection(db, 'posts'), {
+    created_at: serverTimestamp(),
+    artist_id: uid,
+    image: infoPicture,
+    username: artist.username,
+    city_name: artist.city_name,
+    country: artist.country,
+    displayName: artist.displayName,
+    styles,
+    description: dataForm.description,
+  })
+    .then((doc) => {
+      return { doc: doc.id, status: true }
+    })
+    .catch((error) => console.log(error))
 }
 
 // Studio queries
