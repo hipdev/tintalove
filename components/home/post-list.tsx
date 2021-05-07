1680b9d7d749d49b9dcfd7b37f08da24ebefad34
Julián David <julianfullstackdev@gmail.com>
2021-05-06 19:20:47 -0500

    create post item for post list

--- components/home/post-list.tsx
+++ components/home/post-list.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link'
-
 import { MdFilterList } from 'react-icons/md'
-import { FaRegCommentDots } from 'react-icons/fa'
-import { RiHeartLine } from 'react-icons/ri'
+
 import Masonry from 'react-masonry-css'
+import PostItem from './post-item'
 
 const ArtistList = ({ posts }) => {
   console.log(posts, 'los posts')
@@ -41,47 +39,7 @@ const ArtistList = ({ posts }) => {
         columnClassName="my-masonry-grid_column"
       >
         {posts.length > 0 ? (
-          posts.map((post) => (
-            <Link href={`/${post.username}`} key={post.username}>
-              <a>
-                <img
-                  // src="https://via.placeholder.com/309x234"
-                  src={
-                    post?.image?.url
-                      ? `${post.image.url}/tr:pr-true,c-at_max,f-auto,h-235,q-100`
-                      : 'https://via.placeholder.com/309x234'
-                  }
-                  alt=""
-                  className="w-full rounded-sm mb-1 object-cover"
-                />
-                <div className="flex justify-between mt-2">
-                  <div className="flex items-center space-x-2">
-                    <img
-                      src={`${post.artist_picture}/tr:pr-true,c-at_max,f-auto,h-32,q-100`}
-                      className="w-7 h-7 bg-primary rounded-full"
-                    />
-                    <p className="text-white text-sm">
-                      {post.displayName || 'Sin nombre'}
-                    </p>
-                  </div>
-                  <div className="flex space-x-5">
-                    <div className="flex items-center space-x-2 text-white">
-                      <p className="">10</p>
-                      <span>
-                        <FaRegCommentDots />
-                      </span>
-                    </div>
-                    <div className="flex items-center space-x-2 text-white">
-                      <p className="">53</p>
-                      <span>
-                        <RiHeartLine />
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </Link>
-          ))
+          posts.map((post) => <PostItem post={post} />)
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
