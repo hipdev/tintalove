4b32bd7a9df3074690092229221b7297dcfe5a56
Julián David <julianfullstackdev@gmail.com>
2021-05-06 20:12:52 -0500

    update packages, add react sticky node and fix key on list items

--- components/home/post-item.tsx
+++ components/home/post-item.tsx
@@ -5,45 +5,43 @@ import { RiHeartLine } from 'react-icons/ri'
 
 const PostItem = ({ post }) => {
   return (
-    <Link href={`/${post.username}`} key={post.username}>
-      <a>
-        <img
-          // src="https://via.placeholder.com/309x234"
-          src={
-            post?.image?.url
-              ? `${post.image.url}/tr:pr-true,c-at_max,f-auto,h-235,q-100`
-              : 'https://via.placeholder.com/309x234'
-          }
-          alt=""
-          className="w-full rounded-sm mb-1 object-cover"
-        />
-        <div className="flex justify-between mt-2">
-          <div className="flex items-center space-x-2">
-            <img
-              src={`${post.artist_picture}/tr:pr-true,c-at_max,f-auto,h-32,q-100`}
-              className="w-7 h-7 bg-primary rounded-full"
-            />
-            <p className="text-white text-sm">
-              {post.displayName || 'Sin nombre'}
-            </p>
+    <div>
+      <img
+        // src="https://via.placeholder.com/309x234"
+        src={
+          post?.image?.url
+            ? `${post.image.url}/tr:pr-true,c-at_max,f-auto,h-235,q-100`
+            : 'https://via.placeholder.com/309x234'
+        }
+        alt=""
+        className="w-full rounded-sm  object-cover"
+      />
+      <div className="flex justify-between mt-2">
+        <div className="flex items-center space-x-2">
+          <img
+            src={`${post.artist_picture}/tr:pr-true,c-at_max,f-auto,h-32,q-100`}
+            className="w-7 h-7 bg-primary rounded-full"
+          />
+          <p className="text-white text-sm">
+            {post.displayName || 'Sin nombre'}
+          </p>
+        </div>
+        <div className="flex space-x-5">
+          <div className="flex items-center space-x-2 text-white">
+            <p className="">10</p>
+            <span>
+              <FaRegCommentDots />
+            </span>
           </div>
-          <div className="flex space-x-5">
-            <div className="flex items-center space-x-2 text-white">
-              <p className="">10</p>
-              <span>
-                <FaRegCommentDots />
-              </span>
-            </div>
-            <div className="flex items-center space-x-2 text-white">
-              <p className="">53</p>
-              <span>
-                <RiHeartLine />
-              </span>
-            </div>
+          <div className="flex items-center space-x-2 text-white">
+            <p className="">53</p>
+            <span>
+              <RiHeartLine />
+            </span>
           </div>
         </div>
-      </a>
-    </Link>
+      </div>
+    </div>
   )
 }
 
