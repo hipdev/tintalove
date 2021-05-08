603c7ca93c86162dbb02b58c6c24ca094472dea0
Julián David <julianfullstackdev@gmail.com>
2021-05-07 23:07:37 -0500

    component for post comment

--- components/post/post.tsx
+++ components/post/post.tsx
@@ -11,6 +11,7 @@ import { RiHeartLine } from 'react-icons/ri'
 import { BsBookmark } from 'react-icons/bs'
 import { FiSend } from 'react-icons/fi'
 import Link from 'next/link'
+import PostComment from './post-coment'
 
 const Post = ({ postData, postId, artistData }) => {
   console.log(postData, 'la data del post')
@@ -59,13 +60,6 @@ const Post = ({ postData, postId, artistData }) => {
         </div>
         <div className="flex flex-col-reverse lg:flex-row justify-between mb-10">
           <div className="mb-4 mr-3">
-            <div>
-              <p className="text-light-200 mb-2">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Molestiae aut ipsam, dolor qui reprehenderit voluptate nesciunt,
-                labore neque illo, at commodi ab tempore esse reiciendis.
-              </p>
-            </div>
             <div className="flex justify-center lg:justify-start gap-2 mb-5">
               <input
                 type="text"
@@ -79,72 +73,10 @@ const Post = ({ postData, postId, artistData }) => {
                 Comentar
               </button>
             </div>
-            <div className="w-96 flex space-x-4 mb-3">
-              <div className="flex-shrink-0">
-                <img
-                  src="https://via.placeholder.com/45x45"
-                  alt=""
-                  className="rounded-lg"
-                />
-              </div>
-              <div>
-                <div>
-                  <h4 className="text-white font-raleway font-semibold tracking-wide">
-                    Daniel Castillo
-                  </h4>
-                  <p className="text-light-400 text-xs">24 de Marzo</p>
-                </div>
-                <p className="text-light-200 text-sm">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  aliquyam erat,
-                </p>
-              </div>
-            </div>
-            <div className="w-4/5 flex space-x-4 mb-3">
-              <div className="flex-shrink-0">
-                <img
-                  src="https://via.placeholder.com/45x45"
-                  alt=""
-                  className="rounded-lg"
-                />
-              </div>
-              <div>
-                <div>
-                  <h4 className="text-white font-raleway font-semibold tracking-wide">
-                    Daniel Castillo
-                  </h4>
-                  <p className="text-light-400 text-xs">24 de Marzo</p>
-                </div>
-                <p className="text-light-200 text-sm">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  aliquyam erat,
-                </p>
-              </div>
-            </div>
-            <div className="w-4/5 flex space-x-4 mb-3">
-              <div className="flex-shrink-0">
-                <img
-                  src="https://via.placeholder.com/45x45"
-                  alt=""
-                  className="rounded-lg"
-                />
-              </div>
-              <div>
-                <div>
-                  <h4 className="text-white font-raleway font-semibold tracking-wide">
-                    Daniel Castillo
-                  </h4>
-                  <p className="text-light-400 text-xs">24 de Marzo</p>
-                </div>
-                <p className="text-light-200 text-sm">
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  aliquyam erat,
-                </p>
-              </div>
-            </div>
+
+            <PostComment />
+            <PostComment />
+            <PostComment />
           </div>
           <div className="flex-shrink-0 self-center lg:self-start rounded-lg overflow-hidden mb-5 lg:mb-0">
             <div className="w-80 xl:w-96 h-auto bg-ocean_blue-200 px-10 xl:px-13 pt-10 pb-8 rounded-b-lg">
