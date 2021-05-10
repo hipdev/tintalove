03ebf2b9f519504367c1a4e36079024e16f403fe
St1v3l <david-cardona-17@hotmail.com>
2021-05-10 10:03:20 -0500

    working-with-next

---
+++ components/layout-pages/studio-card.tsx
@@ -0,0 +1,50 @@
+import { AiOutlineCalendar } from 'react-icons/ai'
+import { StudioTypes } from 'types/studios'
+
+const StudioCard = ({ studios }: { studios: StudioTypes }) => {
+  return (
+    <div className="w-full h-full bg-ocean_blue-800 p-5 rounded-md">
+      <div className="flex justify-between">
+        <div className="flex flex-wrap gap-5">
+          <div className="bg-black w-14 sm:w-16 h-14 sm:h-16 rounded-xl">
+            <img src="" alt="" />
+          </div>
+          <div>
+            <h1 className="text-2xl font-semibold text-white tracking-wide">
+              {studios.username}
+            </h1>
+            <div className="flex gap-4 justify-between">
+              <div className="flex items-center gap-3 mb-2">
+                <span className="text-green-500 text-xl">
+                  <AiOutlineCalendar />
+                </span>
+                <p className="text-green-500 text-sm">Disponible en 15 días</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-white mb-2 mt-3">
+        <span>Estilos: </span>
+        {studios?.styles ? (
+          studios.styles.toString()
+        ) : (
+          <span>Ninguno registrado</span>
+        )}
+      </p>
+      <div className="grid grid-cols-3 gap-2">
+        <div className="bg-gray-300 h-full w-full rounded-md overflow-hidden">
+          <img src="" alt="" className="h-32 w-full" />
+        </div>
+        <div className="bg-gray-300 h-full w-full rounded-md overflow-hidden">
+          <img src="" alt="" className="h-32 w-full" />
+        </div>
+        <div className="bg-gray-300 h-full w-full rounded-md overflow-hidden">
+          <img src="" alt="" className="h-32 w-full" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StudioCard
--- pages/profile-studios.tsx
+++ pages/profile-studios.tsx
@@ -1,5 +1,36 @@
 import ProfileStudios from 'components/layout-pages/w-profile-studios'
+import { postsToJSON } from 'lib/firebase'
+import { getStudiosInfo } from 'lib/queries/studios'
 
-export default function IndexPage() {
-  return <ProfileStudios />
+export default function ProfileStudiosPage({ studiosData }) {
+  console.log(studiosData, 'esto que es')
+  if (!studiosData) {
+    return <span>No hay artistas registrados</span>
+  }
+  return (
+    <>
+      <ProfileStudios studiosData={studiosData} />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  console.log('YO NO ME VEO EN EL BROWSER')
+  let studiosData = null
+
+  try {
+    const data = await getStudiosInfo()
+    console.log(data, 'esto que')
+
+    studiosData = postsToJSON(data.studios)
+  } catch (error) {
+    console.log(error, 'Error obteniendo la info del artista')
+  }
+
+  return {
+    props: {
+      studiosData,
+    },
+    revalidate: 2,
+  }
 }
