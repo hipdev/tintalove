8c5ccfc8e147f4c64c363f3d2e37a148a6d6aaa1
Julián David <julianfullstackdev@gmail.com>
2021-05-19 14:24:26 -0500

    close list modal when target link

--- components/layout/lists/show-lists.tsx
+++ components/layout/lists/show-lists.tsx
@@ -1,8 +1,14 @@
 import useLists from 'hooks/use-lists'
+import { useStateMachine } from 'little-state-machine'
 import Link from 'next/link'
+import { lists } from 'lib/actions'
 
 const ShowLists = ({ userId }) => {
   const [userLists] = useLists(userId)
+
+  const { state, actions }: any = useStateMachine({
+    lists,
+  })
 
   if (!userLists) return <p className="text-gray-300"> Cargando listas...</p>
 
@@ -13,7 +19,12 @@ const ShowLists = ({ userId }) => {
       <div className="flex flex-col">
         {userLists.map((list) => (
           <Link href={`/list/${list.id}`} key={list.id}>
-            <a className="py-3">{list.list_name}</a>
+            <a
+              className="py-3"
+              onClick={() => actions.lists({ post: null, listOpen: false })}
+            >
+              {list.list_name}
+            </a>
           </Link>
         ))}
       </div>
