f619fc81c4caa85dd430419b7cf3a8190ce83dd1
Julián David <julianfullstackdev@gmail.com>
2021-05-18 19:59:11 -0500

    advance more

--- components/layout/lists/no-list-form.tsx
+++ components/layout/lists/no-list-form.tsx
@@ -5,7 +5,11 @@ import toast from 'react-hot-toast'
 import { addPostToList, createList } from 'lib/queries/lists'
 import { useUserData } from 'hooks/use-user-data'
 
-const NoListForm = () => {
+type Props = {
+  hasList?: boolean
+}
+
+const NoListForm = ({ hasList }: Props) => {
   const [listName, setListName] = useState('')
   const { setTriggerAuth } = useUserData()
 
@@ -22,7 +26,7 @@ const NoListForm = () => {
     e.preventDefault()
     if (list.post?.id) {
       console.log(list.post, 'list post')
-      toast.promise(createList(user, listName, true), {
+      toast.promise(createList(user, listName, hasList ? false : true), {
         loading: 'Creando lista y asignando el tattoo...',
         success: (res: { doc: string; status: boolean }) => {
           console.log(res, 'la res')
@@ -53,8 +57,10 @@ const NoListForm = () => {
   }
 
   return (
-    <>
-      <p className="text-gray-300">Crea tu primera lista</p>
+    <div className="mb-10">
+      <p className="text-gray-300">
+        {hasList ? 'Crear lista' : 'Crea tu primera lista'}
+      </p>
 
       <form onSubmit={handleSubmit} className="text-gray-400 flex flex-col">
         <input
@@ -62,10 +68,13 @@ const NoListForm = () => {
           value={listName}
           placeholder="Nombre de la lista"
           onChange={(e) => setListName(e.target.value)}
+          className="input-primary mt-3"
         />
-        <button type="submit">Crear</button>
+        <button type="submit" className="mt-4 hover:text-primaryHover">
+          Enviar
+        </button>
       </form>
-    </>
+    </div>
   )
 }
 
