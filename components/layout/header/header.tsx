9cce17e088fa96d4819585e2f4b6b44a3f852d0b
St1v3l <david-cardona-17@hotmail.com>
2021-05-21 12:36:45 -0500

    changes responsive desing login header

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -16,45 +16,56 @@ const Header = () => {
 
   const { user } = state
   return (
-    <nav className="h-auto xl:h-20 w-full bg-dark-800 py-4 px-10 md:px-20">
+    <nav className="h-16 md:h-auto xl:h-20 w-full bg-dark-800 py-10 md:py-4 px-5 sm:px-10 lg:px-20">
       <div className="flex flex-wrap md:flex-nowrap justify-center xl:justify-between">
         <div className="w-full flex flex-shrink items-center justify-between md:justify-center xl:justify-start">
-          <Link href="/">
-            <a>
-              {/* <img className="w-52" src="/short-logo.png" /> */}
-              <div className="w-40 relative h-9 mt-2">
-                <Image
-                  // layout="fill"
-                  width={180}
-                  height={35}
-                  src="/short-logo.png"
-                  alt="Picture of the author"
-                />
-              </div>
-            </a>
-          </Link>
-          <span className="text-white text-2xl hidden md:block">
-            <VscMenu />
-          </span>
           {!user?.is_artist && (
             <>
-              <div className="lg:w-full xl:w-7/12 relative hidden lg:flex items-center">
-                <input
-                  type="search"
-                  placeholder="ENCUENTRA TATUAJES Y ARTISTAS INCREIBLES"
-                  className="w-96 lg:w-full xl:w-7/12 h-12 px-5 rounded-l-lg placeholder-black truncate"
-                />
-                <button
-                  type="submit"
-                  className="w-14 h-12 bg-green-500 rounded-r-lg"
-                >
-                  <span className="text-xl text-white flex justify-center">
+              <div className="w-full flex justify-between lg:justify-start items-center">
+                <Link href="/">
+                  <a>
+                    {/* <img className="w-52" src="/short-logo.png" /> */}
+                    <div className="w-40 relative h-9 mt-2">
+                      <Image
+                        // layout="fill"
+                        width={180}
+                        height={35}
+                        src="/short-logo.png"
+                        alt="Picture of the author"
+                      />
+                    </div>
+                  </a>
+                </Link>
+                {/*New elements for tablet resolution*/}
+                <div className="flex items-center">
+                  <span className="text-white text-2xl block lg:hidden px-10">
                     <BsSearch />
                   </span>
-                </button>
+                  <span className="text-white text-3xl block lg:hidden">
+                    <VscMenu />
+                  </span>
+                </div>
+                <span className="text-white text-3xl hidden lg:block px-10">
+                  <VscMenu />
+                </span>
+                <div className="lg:w-full xl:w-7/12 relative hidden lg:flex items-center">
+                  <input
+                    type="search"
+                    placeholder="ENCUENTRA TATUAJES Y ARTISTAS INCREIBLES"
+                    className="w-96 lg:w-full xl:w-7/12 h-12 px-5 rounded-l-lg placeholder-black truncate"
+                  />
+                  <button
+                    type="submit"
+                    className="w-14 h-12 bg-green-500 rounded-r-lg"
+                  >
+                    <span className="text-xl text-white flex justify-center">
+                      <BsSearch />
+                    </span>
+                  </button>
+                </div>
               </div>
-              <div className="flex-grow justify-center xl:justify-end gap-5 py-4 md:py-0 ml-0 xl:ml-3 hidden md:flex mr-5">
-                <div className="flex items-center space-x-2 ml- md:ml-6 lg:ml-0">
+              <div className="flex-grow justify-center xl:justify-end gap-5 py-4 md:py-0 ml-0 xl:ml-3 hidden lg:flex">
+                <div className="flex items-center space-x-2 md:ml-6 lg:ml-2">
                   <span className="text-3xl text-primary">
                     <TiLocationOutline />
                   </span>
@@ -66,14 +77,31 @@ const Header = () => {
                     <option value="">TODO COLOMBIA</option>
                   </select>
                 </div>
+                <SubMenuHeader user={user} />
               </div>
-              <SubMenuHeader user={user} />
             </>
           )}
           {user?.is_artist && (
-            <div className="flex flex-wrap xl:flex-nowrap justify-between w-full">
-              <div className="flex ">
-                <div className="w-28 flex items-center gap-3">
+            <div className="flex flex-wrap xl:flex-nowrap justify-between md:justify-center xl:justify-between w-full">
+              <div className="flex items-center mb-0 md:mb-5 xl:mb-0">
+                <Link href="/">
+                  <a>
+                    {/* <img className="w-52" src="/short-logo.png" /> */}
+                    <div className="w-40 relative h-9 mt-2">
+                      <Image
+                        // layout="fill"
+                        width={180}
+                        height={35}
+                        src="/short-logo.png"
+                        alt="Picture of the author"
+                      />
+                    </div>
+                  </a>
+                </Link>
+                <span className="text-white text-2xl hidden md:block px-10">
+                  <VscMenu />
+                </span>
+                <div className="w-28 items-center gap-3 hidden md:flex">
                   <span className="text-white text-xl">
                     <BsSearch />
                   </span>
@@ -83,7 +111,7 @@ const Header = () => {
                     className="placeholder-white text-white bg-transparent w-28 truncate "
                   />
                 </div>
-                <div className="flex items-center space-x-2 ml- md:ml-6 lg:ml-0">
+                <div className="items-center space-x-2 ml- md:ml-6 lg:ml-0 hidden md:flex">
                   <span className="text-3xl text-white">
                     <TiLocationOutline />
                   </span>
@@ -96,8 +124,17 @@ const Header = () => {
                   </select>
                 </div>
               </div>
-              <div className="flex gap-3 ml-2 ">
-                <div>
+              {/*New elements for tablet resolution*/}
+              <div className="flex items-center">
+                <span className="text-white text-2xl block md:hidden px-0 md:px-10 pr-10 md:pr-0">
+                  <BsSearch />
+                </span>
+                <span className="text-white text-3xl block md:hidden">
+                  <VscMenu />
+                </span>
+              </div>
+              <div className="gap-3 ml-2 hidden md:flex">
+                <div className="ml-10">
                   <div className="flex items-center gap-2 bg-ocean_blue-300 px-2 py-1 rounded-md">
                     <span className="text-green-500 text-2xl">
                       <FiCalendar />
@@ -118,14 +155,6 @@ const Header = () => {
               </div>
             </div>
           )}
-          <div className="flex items-center gap-8">
-            <span className="static text-xl text-white block lg:hidden">
-              <BsSearch />
-            </span>
-            <span className="static text-white text-3xl block md:hidden">
-              <VscMenu />
-            </span>
-          </div>
         </div>
       </div>
     </nav>
