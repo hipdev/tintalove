cdcb0b5906d3513ce1e3ed554fdc214cfd8dcfa5
Julián David <julianfullstackdev@gmail.com>
2021-05-21 16:58:51 -0500

    moving more things

--- pages/studio-account/general.tsx
+++ pages/studio-account/general.tsx
@@ -2,42 +2,36 @@ import GeneralInfo from 'components/studio-account/general-info'
 import GeneralInfoEdit from 'components/studio-account/general-info-edit/general-info-edit'
 import IsAuth from 'components/isAuth'
 import LayoutStepsStudio from 'components/layout-steps/layout-steps-studio'
-import useUser from 'hooks/use-user'
 import useScript from 'hooks/use-script'
+import useUserId from 'hooks/use-user-id'
+import useSWR from 'swr'
+import { getUserInfo } from 'lib/queries/users'
 
 export default function MainInfoPage() {
-  const { state } = useUser()
-  const hasStudio = state?.user?.has_studio
-
+  const { userId } = useUserId()
+  const { data } = useSWR(userId ? userId : null, getUserInfo)
   const status = useScript(
     'https://maps.googleapis.com/maps/api/js?key=AIzaSyA5drETj_sJmO1kGEDEb7tXWzwJb05ipCY&libraries=places'
   )
 
+  if (!data && !data?.user) {
+    return <IsAuth>Cargando data...</IsAuth>
+  }
+
   return (
-    <>
-      {state?.user ? (
-        <LayoutStepsStudio
-          uid={state?.user?.uid}
-          userState={state?.user || null}
-        >
-          {state && state.user && hasStudio ? (
-            <div>
-              {status === 'ready' && (
-                <GeneralInfoEdit
-                  studioId={state?.user?.studio_id || null}
-                  uid={state?.user?.uid || null}
-                />
-              )}
-            </div>
-          ) : (
-            <GeneralInfo uid={state?.user?.uid || null} />
+    <LayoutStepsStudio uid={userId} userState={data.user}>
+      {data.user.has_studio ? (
+        <div>
+          {status === 'ready' && (
+            <GeneralInfoEdit
+              studioId={data.user.studio_id || null}
+              uid={data.user.uid || null}
+            />
           )}
-        </LayoutStepsStudio>
+        </div>
       ) : (
-        <LayoutStepsStudio>
-          <IsAuth>Cargando...</IsAuth>
-        </LayoutStepsStudio>
+        <GeneralInfo uid={data.user.uid || null} />
       )}
-    </>
+    </LayoutStepsStudio>
   )
 }
