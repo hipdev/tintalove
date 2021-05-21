7bee2e0efae6b385b5b5542ec9b1df7d5e54519f
Julián David <julianfullstackdev@gmail.com>
2021-05-21 17:44:28 -0500

    moving more stuff

--- components/home/posts-list.tsx
+++ components/home/posts-list.tsx
@@ -1,8 +1,14 @@
+import useUserId from 'hooks/use-user-id'
+import { getUserInfo } from 'lib/queries/users'
 import { MdFilterList } from 'react-icons/md'
 import Masonry from 'react-masonry-css'
+import useSWR from 'swr'
 import PostItem from './post-item'
 
 const PostListHome = ({ posts }) => {
+  const { userId } = useUserId()
+  const { data } = useSWR(userId ? userId : null, getUserInfo)
+
   const breakpointColumnsObj = {
     default: 6,
     1600: 4,
@@ -36,27 +42,57 @@ const PostListHome = ({ posts }) => {
         columnClassName="my-masonry-grid_column"
       >
         {posts.length > 0 ? (
-          posts.map((post) => <PostItem key={Math.random()} post={post} />)
+          posts.map((post) => (
+            <PostItem
+              key={Math.random()}
+              post={post}
+              user={data?.user || null}
+            />
+          ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
         {posts.length > 0 ? (
-          posts.map((post) => <PostItem key={Math.random()} post={post} />)
+          posts.map((post) => (
+            <PostItem
+              key={Math.random()}
+              post={post}
+              user={data?.user || null}
+            />
+          ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
         {posts.length > 0 ? (
-          posts.map((post) => <PostItem key={Math.random()} post={post} />)
+          posts.map((post) => (
+            <PostItem
+              key={Math.random()}
+              post={post}
+              user={data?.user || null}
+            />
+          ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
         {posts.length > 0 ? (
-          posts.map((post) => <PostItem key={Math.random()} post={post} />)
+          posts.map((post) => (
+            <PostItem
+              key={Math.random()}
+              post={post}
+              user={data?.user || null}
+            />
+          ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
         {posts.length > 0 ? (
-          posts.map((post) => <PostItem key={Math.random()} post={post} />)
+          posts.map((post) => (
+            <PostItem
+              key={Math.random()}
+              post={post}
+              user={data?.user || null}
+            />
+          ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
