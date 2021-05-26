401bc127ebddd58338bddc2001fe96838eaa1681
Julián David <julianfullstackdev@gmail.com>
2021-05-26 17:10:53 -0500

    footer fixed with state

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,49 +1,69 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import { AiOutlineInstagram } from 'react-icons/ai'
 import { FaFacebookSquare } from 'react-icons/fa'
+import { MdHelpOutline } from 'react-icons/md'
 
 const Footer = () => {
+  const [showFooter, setShowFooter] = useState(false)
   return (
-    <footer className="flex flex-col lg:flex-row w-full justify-between px-8 sm:px-20 bg-dark-800 items-center pb-3 pt-20 uppercase">
-      <div className="w-48 lg:w-60 mb-3 lg:mb-0">
-        <Link href="/">
-          <a className="relative bottom-3">
-            <img className="w-48 relative top-2" src="/newlogo.png" />
-          </a>
-        </Link>
+    <>
+      <footer
+        className={`${showFooter ? 'block' : 'hidden'} fixed bottom-0 w-full `}
+      >
+        <div className="flex flex-col lg:flex-row w-full justify-between px-2 sm:px-20 bg-dark-800 items-center pb-3 pt-2 uppercase">
+          <div className="w-48 lg:w-60 mb-3 lg:mb-0">
+            <Link href="/">
+              <a className="relative bottom-3">
+                <img className="w-48 relative top-4" src="/newlogo.png" />
+              </a>
+            </Link>
+          </div>
+          <div className="w-full flex flex-wrap justify-end leading-7">
+            <Link href="/">
+              <a className="text-sm sm:text-base text-white mx-3">
+                Política de privacidad
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="text-sm sm:text-base text-white mx-3">
+                Términos y condiciones
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="text-sm sm:text-base text-white mx-3">
+                Soy un artista
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="text-sm sm:text-base text-white mx-3">
+                Para estudios
+              </a>
+            </Link>
+
+            <Link href="/">
+              <a className="text-white text-2xl mx-3">
+                <AiOutlineInstagram />
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="text-white text-2xl mx-3">
+                <FaFacebookSquare />
+              </a>
+            </Link>
+          </div>
+        </div>
+      </footer>
+
+      <div
+        className="fixed bottom-5 right-5 text-gray-200 text-4xl cursor-pointer"
+        onClick={() => setShowFooter(!showFooter)}
+      >
+        <MdHelpOutline />
       </div>
-      <div className="w-full flex flex-wrap justify-end leading-7">
-        <Link href="/">
-          <a className="text-sm sm:text-base text-white mx-3">
-            Política de privacidad
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="text-sm sm:text-base text-white mx-3">
-            Términos y condiciones
-          </a>
-        </Link>
-
-        <Link href="/">
-          <a className="text-sm sm:text-base text-white mx-3">Soy un artista</a>
-        </Link>
-
-        <Link href="/">
-          <a className="text-sm sm:text-base text-white mx-3">Para estudios</a>
-        </Link>
-
-        <Link href="/">
-          <a className="text-white text-2xl mx-3">
-            <AiOutlineInstagram />
-          </a>
-        </Link>
-        <Link href="/">
-          <a className="text-white text-2xl mx-3">
-            <FaFacebookSquare />
-          </a>
-        </Link>
-      </div>
-    </footer>
+    </>
   )
 }
 
