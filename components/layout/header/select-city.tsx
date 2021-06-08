49e66aceb8a90c77e1f440f192abe53071361fda
Julián David <julianfullstackdev@gmail.com>
2021-06-08 17:11:29 -0500

    chagnes

--- components/layout/header/select-city.tsx
+++ components/layout/header/select-city.tsx
@@ -1,27 +1,97 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+
 import { getCities } from 'lib/queries/general'
 import useSWR from 'swr'
+import { HiOutlineSelector } from 'react-icons/hi'
+import { AiOutlineCheck } from 'react-icons/ai'
+import { TiLocationOutline } from 'react-icons/ti'
 
 const SelectCity = () => {
   const { data } = useSWR(['get-cities', 'Colombia'], getCities)
+
+  const [selected, setSelected] = useState()
 
   if (!data) return <span>...</span>
 
   return (
     data && (
-      <select
-        name=""
-        className="bg-transparent text-white font-light font-raleway underline focus:outline-none"
-      >
-        <option value="">TODO COLOMBIA</option>
-        {data.cities.map((city) => {
-          console.log(city, 'la ciudad')
-          return (
-            <option key={city.city_hash} value={city.city_hash}>
-              {city.city_name}
-            </option>
-          )
-        })}
-      </select>
+      <>
+        <Listbox value={selected} onChange={setSelected}>
+          {({ open }) => (
+            <>
+              <Listbox.Label className="block text-sm font-medium">
+                <TiLocationOutline className="text-3xl text-primary" />
+              </Listbox.Label>
+              <div className="mt-1 relative">
+                <Listbox.Button
+                  className="bg-transparent relative w-full rounded-md text-gray-200
+                shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary"
+                >
+                  <span className="block truncate">Todo Colombia</span>
+                  <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <HiOutlineSelector
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
+
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options
+                    static
+                    className="absolute z-30 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                  >
+                    {data.cities.map((city) => (
+                      <Listbox.Option
+                        key={city.city_hash}
+                        className={({ active }) =>
+                          (active ? 'text-white bg-primary' : 'text-gray-900') +
+                          ' cursor-default select-none relative py-2 pl-3 pr-9'
+                        }
+                        value={city}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <span
+                              className={
+                                (selected ? 'font-semibold' : 'font-normal') +
+                                ' block truncate'
+                              }
+                            >
+                              {city.city_name}
+                            </span>
+
+                            {selected ? (
+                              <span
+                                className={
+                                  (active ? 'text-white' : 'text-primary') +
+                                  ' absolute inset-y-0 right-0 flex items-center pr-4'
+                                }
+                              >
+                                <AiOutlineCheck
+                                  className="h-5 w-5"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </>
+          )}
+        </Listbox>
+      </>
     )
   )
 }
