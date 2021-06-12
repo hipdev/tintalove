28b6f46a22acfd2f85a8ddf90562a0c80c5889f8
Julián David <julianfullstackdev@gmail.com>
2021-06-12 18:47:18 -0500

    location ready

---
+++ pages/location/[location].tsx
@@ -0,0 +1,53 @@
+import Layout from 'components/layout/layout'
+import { postsToJSON, postToJSON } from 'lib/firebase'
+import { getPostsInfo } from 'lib/queries/posts'
+import Home from 'components/home/home'
+import { getCitiesPaths, getLatLngFromCityId } from 'lib/queries/general'
+import { getPostsByCity } from 'lib/queries/geo'
+
+export default function TattoosPage({ postsData }) {
+  return <Layout>{postsData && <Home posts={postsData} />}</Layout>
+}
+
+export async function getStaticPaths() {
+  const citiesIds = await getCitiesPaths()
+  const pathsLocations = citiesIds.map((doc: any) => ({
+    params: {
+      location: doc.id,
+    },
+  }))
+
+  const withAll = [...pathsLocations, { params: { location: 'Colombia' } }]
+
+  return {
+    paths: withAll,
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  let postsData = null
+
+  console.log(params, 'params')
+
+  const splitLocation = params.location.split('~')
+  const latLng = [parseFloat(splitLocation[1]), parseFloat(splitLocation[2])]
+
+  // console.log(latLng, 'lat y lng')
+
+  if (params.location == 'Colombia') {
+    const { posts } = await getPostsInfo()
+    postsData = postsToJSON(posts)
+  } else {
+    const { latLng } = await getLatLngFromCityId(params.location)
+    const { posts } = await getPostsByCity(latLng)
+    postsData = postsToJSON(posts)
+  }
+
+  return {
+    props: {
+      postsData,
+    },
+    revalidate: 20,
+  }
+}
