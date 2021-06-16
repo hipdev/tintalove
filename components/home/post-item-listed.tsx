1584d9a610aead3442bdf7f87b45c8ac459a8e34
Julián David <julianfullstackdev@gmail.com>
2021-06-16 16:15:30 -0500

    replace modal from delete from list

--- components/home/post-item-listed.tsx
+++ components/home/post-item-listed.tsx
@@ -1,4 +1,5 @@
-import Modal from 'react-modal'
+import { useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import { useStateMachine } from 'little-state-machine'
 import { RiHeart3Fill, RiHeartLine } from 'react-icons/ri'
 import { lists } from 'lib/actions'
@@ -7,9 +8,6 @@ import toast from 'react-hot-toast'
 import { isPostListed, removePostFromList } from 'lib/queries/lists'
 import { UserState } from 'types/user'
 import useSWR from 'swr'
-import { useState } from 'react'
-
-Modal.setAppElement('#__next')
 
 const PostItemListed = ({
   post,
@@ -74,48 +72,63 @@ const PostItemListed = ({
 
   return (
     <>
-      <Modal
-        isOpen={isOpen}
-        // style={customStyles}
-        style={{
-          overlay: {
-            position: 'fixed',
-            backgroundColor: 'rgb(8 10 18 / 98%)',
-            top: 0,
-            right: 0,
-            left: 0,
-            bottom: 0,
-            zIndex: 100,
-          },
-          content: {
-            background: 'transparent',
-            border: 'none',
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-          },
-        }}
-        onRequestClose={() => setIsOpen(false)}
-        contentLabel="Post modal"
-      >
-        <div className="bg-black container mx-auto w-448 h-1/2 p-10">
-          <h2 className="text-gray-400">
-            ¿Estas seguro que deseas eliminar este post?
-          </h2>
-          <div className="text-gray-400 flex justify-center mt-5 text-xl">
-            <button
-              className="mr-5 border px-4 py-1 rounded-md"
-              onClick={removeFromList}
+      <Transition.Root show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed z-10 inset-0 overflow-y-auto"
+          open={isOpen}
+          onClose={setIsOpen}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
             >
-              Eliminar
-            </button>
-            <button onClick={() => setIsOpen(false)}>Cancelar</button>
+              <Dialog.Overlay className="absolute inset-0 bg-gray-800 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block w-448 align-bottom rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle  sm:p-6">
+                <div className="bg-black container mx-auto w-448 h-1/2 p-10">
+                  <h2 className="text-gray-400">
+                    ¿Estas seguro que deseas eliminar este post?
+                  </h2>
+                  <div className="text-gray-400 flex justify-center mt-5 text-xl">
+                    <button
+                      className="mr-5 border px-4 py-1 rounded-md"
+                      onClick={removeFromList}
+                    >
+                      Eliminar
+                    </button>
+                    <button onClick={() => setIsOpen(false)}>Cancelar</button>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
           </div>
-        </div>
-      </Modal>
+        </Dialog>
+      </Transition.Root>
 
       {data?.listed ? (
         <span className="cursor-pointer" onClick={() => setIsOpen(true)}>
