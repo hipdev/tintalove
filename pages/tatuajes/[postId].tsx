11dace5061477583660662a6898c4efb9bf03451
Julián David <julianfullstackdev@gmail.com>
2021-06-18 19:04:48 -0500

    add new modal

--- pages/tatuajes/[postId].tsx
+++ pages/tatuajes/[postId].tsx
@@ -1,5 +1,7 @@
 import Script from 'next/script'
 import Layout from 'components/layout/layout'
+import { useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import { postsToJSON, postToJSON } from 'lib/firebase'
 import {
   getMorePostFromArtist,
@@ -27,34 +29,86 @@ export default function TattoosPage({
       <Script src="https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js" />
       <Script src="https://www.gstatic.com/firebasejs/8.6.2/firebase-firestore.js" />
       {postData && artistData && (
-        <Modal
-          isOpen={!(router.query.postId == 'all')}
-          overlayClassName="fixed left-0 right-0 bottom-0 overflow-auto"
-          className="bg-transparent  overflow-auto w-full px-3 sm:px-7 absolute"
-          // style={customStyles}
-          style={{
-            overlay: {
-              backgroundColor: '#0b0e19',
-              top: 80,
-              zIndex: 10,
-            },
-            content: {
-              background: 'transparent',
-              border: 'none',
-              top: 0,
-            },
-          }}
-          onRequestClose={() => router.back()}
-          contentLabel="Post modal"
-        >
-          <Post
-            postData={postData}
-            artistData={artistData}
-            commentsData={commentsData}
-            morePostsArtist={morePostsArtist}
-            closeModal={() => router.back()}
-          />
-        </Modal>
+        <>
+          {/* <Modal
+            isOpen={!(router.query.postId == 'all')}
+            overlayClassName="fixed left-0 right-0 bottom-0 overflow-auto"
+            className="bg-transparent  overflow-auto w-full px-3 sm:px-7 absolute"
+            // style={customStyles}
+            style={{
+              overlay: {
+                backgroundColor: '#0b0e19',
+                top: 80,
+                zIndex: 10,
+              },
+              content: {
+                background: 'transparent',
+                border: 'none',
+                top: 0,
+              },
+            }}
+            onRequestClose={() => router.back()}
+            contentLabel="Post modal"
+          >
+            <Post
+              postData={postData}
+              artistData={artistData}
+              commentsData={commentsData}
+              morePostsArtist={morePostsArtist}
+              closeModal={() => router.back()}
+            />
+          </Modal> */}
+          <Transition.Root show={!(router.query.postId == 'all')} as={Fragment}>
+            <Dialog
+              as="div"
+              static
+              className="fixed z-10 inset-0 overflow-y-auto"
+              open={!(router.query.postId == 'all')}
+              onClose={() => router.back()}
+            >
+              <div className="flex  pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Dialog.Overlay className="fixed inset-0 bg-dark-800 " />
+                </Transition.Child>
+
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                  className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                  enterTo="opacity-100 translate-y-0 sm:scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                  leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                >
+                  <div className="inline-block h-screen align-bottom bg-transparent rounded-lg pt-5 pb-4 text-left overflow-y-scroll shadow-xl transform transition-all sm:align-middle  sm:w-full sm:p-6 md:w-5/5">
+                    <Post
+                      postData={postData}
+                      artistData={artistData}
+                      commentsData={commentsData}
+                      morePostsArtist={morePostsArtist}
+                      closeModal={() => router.back()}
+                    />
+                  </div>
+                </Transition.Child>
+              </div>
+            </Dialog>
+          </Transition.Root>
+        </>
       )}
       {!postData && (
         <p className="h-screen bg-dark-800 pt-32 text-4xl text-center font-bold text-gray-200">
