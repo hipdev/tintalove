f5c50ebb3088ac4e6e7767fe9733eedeca3cbc8c
Julián David <julianfullstackdev@gmail.com>
2021-06-22 23:24:48 -0500

    finish more pictures for artists

--- components/artist-account/photos/more-pictures-list.tsx
+++ components/artist-account/photos/more-pictures-list.tsx
@@ -1,38 +1,27 @@
-import { getArtistsPictures } from 'lib/queries/artists'
 import Image from 'next/image'
-import useSWR from 'swr'
 
 const myLoader = ({ src, width, quality }) => {
   return `${src}/tr:pr-true,w-${width},q-${quality || 75}`
 }
 
-const MorePicturesList = ({ artist }) => {
-  const { data }: any = useSWR(
-    ['getArtistPictures', artist?.uid],
-    getArtistsPictures
-  )
-
+const MorePicturesList = ({ artist, pictures }) => {
   return (
-    <div>
-      {data?.pictures ? (
-        <div className="grid grid-cols-3 gap-5">
-          {data.pictures.map((pic) => (
-            <div className="relative h-40">
-              <Image
-                key={pic.fileId}
-                loader={myLoader}
-                src={pic?.url}
-                alt="Artist photo"
-                layout="fill"
-                // width={600}
-                // height={500}
-                sizes="100%"
-                className="w-full rounded-md  object-cover"
-              />
-            </div>
-          ))}
+    <div className="grid grid-cols-3 gap-5">
+      {pictures.map((pic) => (
+        <div className="relative h-40" key={pic.fileId}>
+          <Image
+            key={pic.fileId}
+            loader={myLoader}
+            src={pic?.url}
+            alt="Artist photo"
+            layout="fill"
+            // width={600}
+            // height={500}
+            sizes="100%"
+            className="w-full rounded-md  object-cover"
+          />
         </div>
-      ) : null}
+      ))}
     </div>
   )
 }
