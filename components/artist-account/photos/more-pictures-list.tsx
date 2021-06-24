7b330be8970ff428e13b08fcb725554473454617
Julián David <julianfullstackdev@gmail.com>
2021-06-24 09:02:23 -0500

    more changes

--- components/artist-account/photos/more-pictures-list.tsx
+++ components/artist-account/photos/more-pictures-list.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { AiOutlineDelete } from 'react-icons/ai'
 
 const myLoader = ({ src, width, quality }) => {
   return `${src}/tr:pr-true,w-${width},q-${quality || 75}`
@@ -8,7 +9,7 @@ const MorePicturesList = ({ artist, pictures }) => {
   return (
     <div className="grid grid-cols-3 gap-5">
       {pictures.map((pic) => (
-        <div className="relative h-40" key={pic.fileId}>
+        <div className="relative h-40 group" key={pic.fileId}>
           <Image
             key={pic.fileId}
             loader={myLoader}
@@ -20,6 +21,11 @@ const MorePicturesList = ({ artist, pictures }) => {
             sizes="100%"
             className="w-full rounded-md  object-cover"
           />
+
+          <div className="group-hover:absolute right-1 bottom-2">
+            <AiOutlineDelete className="text-2xl text-primary cursor-pointer" />
+          </div>
+          <div className="group-hover:absolute w-full bottom-0"></div>
         </div>
       ))}
     </div>
