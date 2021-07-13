a190a6c532f8d6ff34a8170be947401367618b03
Julián David <julianfullstackdev@gmail.com>
2021-07-13 09:04:37 -0500

    different structure

--- components/post/PostPortrait.tsx
+++ components/post/PostPortrait.tsx
@@ -5,12 +5,14 @@ import { useState } from 'react'
 import PostAside from './create/PostAside'
 import Image from 'next/image'
 import { UserState } from 'types/user'
+import { BsArrowLeft } from 'react-icons/bs'
 
 const loaderPost = ({ src, quality }: any) => {
   return `${src}/tr:pr-true,c-at_max,f-auto,q-${quality || 75}`
 }
 
 const PostPortrait = ({
+  closeModal,
   user,
   postData,
   artistData,
@@ -20,6 +22,7 @@ const PostPortrait = ({
   artistData: ArtistTypes
   commentsData: any
   user: UserState
+  closeModal: any
 }) => {
   const [totalComments, setTotalComments] = useState(
     postData.counter_comments || 0
@@ -27,11 +30,33 @@ const PostPortrait = ({
 
   return (
     <div className="flex flex-col xl:flex-row items-center xl:justify-between">
-      <div className="mb-5 flex justify-center xl:justify-start w-full sm:w-3/5 md:w-3/5">
-        <div className="mr-3 hidden sm:block">
-          <PostAside postData={postData} />
+      <div className="w-full sm:w-3/5 md:w-3/5">
+        <div className="w-full xl:max-w-3xl flex flex-wrap justify-between mb-8">
+          <div className="flex items-center gap-3">
+            <div className="flex flex-col mb-2">
+              <h1 className="text-white text-2xl font-semibold font-raleway tracking-wide">
+                {postData.description || 'Sin descripción'}
+              </h1>
+              <p className="text-light-200 text-sm self-end">
+                #Realismo #Color #Payaso #Retrato
+              </p>
+            </div>
+          </div>
+          <button
+            className="flex items-center gap-3 text-white focus:outline-none"
+            onClick={closeModal}
+          >
+            <span className="text-3xl">
+              <BsArrowLeft />
+            </span>
+            Volver
+          </button>
         </div>
-        {/* <img
+        <div className="mb-5 flex justify-center xl:justify-start ">
+          <div className="mr-3 hidden sm:block">
+            <PostAside postData={postData} />
+          </div>
+          {/* <img
           // src="https://via.placeholder.com/1100x621"
           src={
             postData?.image?.url
@@ -41,30 +66,31 @@ const PostPortrait = ({
           alt=""
           className=" object-cover rounded-lg  md:max-w-lg lg:max-w-2xl"
         /> */}
-        <div className="aspect-w-3 aspect-h-4 relative w-full h-448">
-          <Image
-            loader={loaderPost}
-            src={postData?.image?.url}
-            alt="Artist photo"
-            layout="fill"
-            // width={600}
-            // height={500}
-            sizes="100%"
-            quality={100}
-            className="w-full rounded-md  object-cover max-h-96"
-          />
-        </div>
+          <div className="aspect-w-3 aspect-h-4 relative w-full h-448">
+            <Image
+              loader={loaderPost}
+              src={postData?.image?.url}
+              alt="Artist photo"
+              layout="fill"
+              // width={600}
+              // height={500}
+              sizes="100%"
+              quality={100}
+              className="w-full rounded-md  object-cover max-h-96"
+            />
+          </div>
 
-        {/* <img
+          {/* <img
           alt=" "
           className=""
           loading="lazy"
           src="https://i.pinimg.com/236x/fb/d6/ce/fbd6ce4c83fd8d13dcdac1a9b72595b0.jpg"
           srcset="https://i.pinimg.com/236x/fb/d6/ce/fbd6ce4c83fd8d13dcdac1a9b72595b0.jpg 1x, https://i.pinimg.com/474x/fb/d6/ce/fbd6ce4c83fd8d13dcdac1a9b72595b0.jpg 2x, https://i.pinimg.com/736x/fb/d6/ce/fbd6ce4c83fd8d13dcdac1a9b72595b0.jpg 3x, https://i.pinimg.com/originals/fb/d6/ce/fbd6ce4c83fd8d13dcdac1a9b72595b0.jpg 4x"
         /> */}
+        </div>
       </div>
 
-      <div className="flex flex-grow flex-col-reverse self-center xl:self-start w-full xl:w-1/2 ml-0 xl:ml-44 overflow-hidden overflow-ellipsis">
+      <div className="flex flex-grow flex-col-reverse self-center xl:self-start w-full xl:w-1/5 ml-0 xl:ml-44 overflow-hidden overflow-ellipsis">
         <PostsComments
           postId={postData.id}
           user={user || null}
