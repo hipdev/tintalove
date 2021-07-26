7f0981ece8524053289f41c5e3dbcb058b1321da
Julián David <julianfullstackdev@gmail.com>
2021-07-26 18:20:51 -0500

    remove some unused clgs to reduce build time

--- pages/artists.tsx
+++ pages/artists.tsx
@@ -3,7 +3,6 @@ import { postsToJSON } from 'lib/firebase'
 import { getArtistsInfo } from 'lib/queries/artists'
 
 export default function ArtistPage({ artistsData }) {
-  console.log(artistsData, 'esto que es')
   if (!artistsData) {
     return <span>No hay artistas registrados</span>
   }
@@ -15,12 +14,10 @@ export default function ArtistPage({ artistsData }) {
 }
 
 export async function getStaticProps() {
-  console.log('YO NO ME VEO EN EL BROWSER')
   let artistsData = null
 
   try {
     const data = await getArtistsInfo()
-    console.log(data, 'esto que')
 
     artistsData = postsToJSON(data.artists)
   } catch (error) {
