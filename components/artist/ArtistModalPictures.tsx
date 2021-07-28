4c0682e4e045d4ba1067e9f9b81020df28735f80
Julián David <julianfullstackdev@gmail.com>
2021-07-28 13:41:07 -0500

    ready modal with one picture

--- components/artist/ArtistModalPictures.tsx
+++ components/artist/ArtistModalPictures.tsx
@@ -47,8 +47,8 @@ const ArtistModalPictures = ({
             <div className="absolute mx-auto left-0 right-0 bg-white mt-10 w-5/6 sm:w-1/2 2xl:w-1/3 top-5 flex flex-col items-center">
               <button
                 type="button"
-                className="absolute text-white -top-10 right-0  text-2xl sm:text-4xl z-10"
-                // onClick={() => removeFromList()}
+                className="absolute text-white hover:text-gray-300 -top-10 right-0  text-2xl sm:text-4xl z-10"
+                onClick={() => setOpenModalPics(false)}
               >
                 <IoMdCloseCircle />
               </button>
