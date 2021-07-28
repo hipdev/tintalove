c21ea28d3ce88197f02f4e39f1cefb268b3a7e5c
Julián David <julianfullstackdev@gmail.com>
2021-07-28 13:03:55 -0500

    fix photo issues with update in profile

--- components/artist/ArtistModalPictures.tsx
+++ components/artist/ArtistModalPictures.tsx
@@ -1,7 +1,12 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
+import { IoMdCloseCircle } from 'react-icons/io'
 
-const ArtistModalPictures = ({ openModalPics, setOpenModalPics }: any) => {
+const ArtistModalPictures = ({
+  openModalPics,
+  setOpenModalPics,
+  artistPics,
+}: any) => {
   return (
     <Transition.Root show={openModalPics} as={Fragment}>
       <Dialog
@@ -11,7 +16,7 @@ const ArtistModalPictures = ({ openModalPics, setOpenModalPics }: any) => {
         open={openModalPics}
         onClose={() => setOpenModalPics(false)}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center min-h-screen pt-4 px-4 pb-20 text-center sm:p-0">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -21,16 +26,9 @@ const ArtistModalPictures = ({ openModalPics, setOpenModalPics }: any) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          {/* This element is to trick the browser into centering the modal contents. */}
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -40,34 +38,19 @@ const ArtistModalPictures = ({ openModalPics, setOpenModalPics }: any) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-              <div>
-                {/* <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-              <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-            </div> */}
-                <div className="mt-3 text-center sm:mt-5">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg leading-6 font-medium text-gray-900"
-                  >
-                    Eliminar de tu lista
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Esta seguro de eliminar este tattoo ?
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5 sm:mt-6">
-                <button
-                  type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary text-base font-medium text-white hover:bg-primaryHover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary sm:text-sm"
-                  // onClick={() => removeFromList()}
-                >
-                  Sí, eliminar.
-                </button>
-              </div>
+            <div className="absolute w-auto mx-auto left-0 right-0 bg-white h-1/2 mt-10">
+              <button
+                type="button"
+                className=""
+                // onClick={() => removeFromList()}
+              >
+                <IoMdCloseCircle />
+              </button>
+              {artistPics?.length == 0 ? (
+                <div>La foto grande</div>
+              ) : (
+                <div>Slider de fotos</div>
+              )}
             </div>
           </Transition.Child>
         </div>
