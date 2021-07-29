7ffeafc6fa2d9d780c03f8cdc59eeb66fe39b3ca
Julián David <julianfullstackdev@gmail.com>
2021-07-28 19:58:31 -0500

    almost ready modal of pics

--- components/artist/ArtistModalPictures.tsx
+++ components/artist/ArtistModalPictures.tsx
@@ -46,7 +46,7 @@ const ArtistModalPictures = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="absolute mx-auto left-0 right-0 bg-white mt-10 w-5/6 sm:w-1/2 2xl:w-1/3 top-5 flex flex-col items-center">
+            <div className="absolute mx-auto left-0 right-0 bg-transparent justify-center h-full mt-10 w-5/6 sm:w-1/2 2xl:w-2/3 top-5 flex flex-col items-center">
               <button
                 type="button"
                 className="absolute text-white hover:text-gray-300 -top-10 right-0  text-2xl sm:text-4xl z-10"
@@ -88,18 +88,21 @@ const ArtistModalPictures = ({
                     <Carousel
                       swipeable
                       useKeyboardArrows
-                      autoFocus={true}
+                      autoFocus
                       stopOnHover
+                      showThumbs={false}
+                      showStatus={false}
+                      dynamicHeight
+                      // showIndicators={false}
                     >
                       <div>
                         <img src={profilePicture} />
                       </div>
-                      <div>
-                        <img src={profilePicture} />
-                      </div>
-                      <div>
-                        <img src={profilePicture} />
-                      </div>
+                      {artistPics?.map((pic) => (
+                        <div key={pic.id}>
+                          <img src={pic.url} />
+                        </div>
+                      ))}
                     </Carousel>
                   )}
                 </div>
