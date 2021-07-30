982310f729f58747c812edd961c4f150461d5410
Julián David <julianfullstackdev@gmail.com>
2021-07-30 16:58:23 -0500

    loadmap without issues on main info for artists

--- pages/artist/main-info.tsx
+++ pages/artist/main-info.tsx
@@ -6,10 +6,28 @@ import useUserId from 'hooks/use-user-id'
 import { getUserInfo } from 'lib/queries/users'
 import Script from 'next/script'
 import useSWR from 'swr'
+import { useState } from 'react'
+import { Loader } from '@googlemaps/js-api-loader'
+
+const loader = new Loader({
+  apiKey: 'AIzaSyA5drETj_sJmO1kGEDEb7tXWzwJb05ipCY', // api key de google maps
+  libraries: ['places'],
+})
 
 export default function MainInfoPage() {
   const { userId } = useUserId()
   const { data } = useSWR(userId ? userId : null, getUserInfo)
+
+  const [loadMap, setLoadMap] = useState(false)
+
+  loader
+    .load()
+    .then(() => {
+      setLoadMap(true)
+    })
+    .catch((e) => {
+      console.log('error loading Google Maps API')
+    })
 
   if (!data && !data?.user) {
     return <IsAuth>Cargando data...</IsAuth>
@@ -17,12 +35,14 @@ export default function MainInfoPage() {
   return (
     <>
       <LayoutStepsArtist uid={userId} user={data.user}>
-        <Script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5drETj_sJmO1kGEDEb7tXWzwJb05ipCY&libraries=places" />
-
-        {data.user.is_artist ? (
-          <MainInfoEdit uid={data.user.uid || null} />
-        ) : (
-          <MainInfo uid={data.user.uid || null} />
+        {loadMap && (
+          <>
+            {data.user.is_artist ? (
+              <MainInfoEdit uid={data.user.uid || null} />
+            ) : (
+              <MainInfo uid={data.user.uid || null} />
+            )}
+          </>
         )}
       </LayoutStepsArtist>
     </>
