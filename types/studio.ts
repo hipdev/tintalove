88f0b2fb320e48935084ba4f16541094c413c6e1
Julián David <julianfullstackdev@gmail.com>
2021-08-02 14:57:23 -0500

    profile studio almost ready

--- types/studio.ts
+++ types/studio.ts
@@ -10,7 +10,7 @@ export type StudioTypes = {
   contact_way?: string
   created_at?: Timestamp
   updated_at?: Timestamp
-  displayName?: string
+  studio_name?: string
   formatted_address?: string
   place_id?: string
   username?: string
@@ -25,6 +25,7 @@ export type StudioTypes = {
   facebook?: string
   instagram?: string
   work_as?: string
+  dataLocation: DataLocation
 } | null
 
 type ProfilePicture = {
@@ -34,4 +35,17 @@ type ProfilePicture = {
   size: number
   thumbnailUrl: string
   url: string
+}
+
+type DataLocation = {
+  city_name: string
+  formatted_address: string
+  geohash: string
+  place_id: string
+  _geoloc: AlgoliaGeolocation
+}
+
+type AlgoliaGeolocation = {
+  lat: number
+  lng: number
 }
