f2405e6f66ef4b85918308a5be388903cabab1bf
Julián David <julianfullstackdev@gmail.com>
2021-08-05 15:38:52 -0500

    advanced much more in algolia filter

--- components/home/layout/HomeFilter.tsx
+++ components/home/layout/HomeFilter.tsx
@@ -1,48 +1,52 @@
 import { getAlgoliaResults } from '@algolia/autocomplete-js'
-import algoliasearch from 'algoliasearch'
-import { Autocomplete } from 'components/algolia/autocomplete'
+import algoliasearch from 'algoliasearch/lite'
 import { ProductItem } from 'components/algolia/ProductItem'
 import '@algolia/autocomplete-theme-classic'
+import AutocompleteStudio from 'components/artist-account/working-info/AutocompleteStudio'
 
 const appId = 'JE20HAUJXG'
 const apiKey = 'db9dbba9f07212053022eec3e364876a'
 const searchClient = algoliasearch(appId, apiKey)
 
-const HomeFilter = () => {
+const SelectStudio = () => {
   return (
-    <div className="relative h-1/4 flex flex-col justify-center items-center mt-24">
-      <div className="mb-10">
-        <img src="/newlogo.png" className="w-72" />
-      </div>
-      <div className="relative w-full md:w-2/4 lg:w-2/5 xl:w-2/6 flex justify-center items-center mb-6 px-3">
-        <Autocomplete
-          // openOnFocus={true}
-          placeholder="ENCUENTRA TATUAJES Y ARTISTAS INCREÍBLES"
-          getSources={({ query }) => [
-            {
-              sourceId: 'artists',
-              getItems() {
-                return getAlgoliaResults({
-                  searchClient,
-                  queries: [
-                    {
-                      indexName: 'tintalove_dev',
-                      query,
-                    },
-                  ],
-                })
+    <div>
+      <AutocompleteStudio
+        openOnFocus={true}
+        getSources={({ query }) => [
+          {
+            sourceId: 'studios',
+            getItems() {
+              return getAlgoliaResults({
+                searchClient,
+                queries: [
+                  {
+                    indexName: 'tintalove_studios',
+                    query,
+                  },
+                  {
+                    indexName: 'tintalove_artists',
+                    query,
+                  },
+                ],
+              })
+            },
+            templates: {
+              item({ item, components }) {
+                return <ProductItem hit={item} components={components} />
               },
-              templates: {
-                item({ item, components }) {
-                  return <ProductItem hit={item} components={components} />
-                },
+              footer() {
+                return 'soy el pie'
+              },
+              noResults() {
+                return 'Sin resultados'
               },
             },
-          ]}
-        />
-      </div>
+          },
+        ]}
+      />
     </div>
   )
 }
 
-export default HomeFilter
+export default SelectStudio
