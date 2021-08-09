23b694cb9fef3377892aed49672aae77ee8a713d
Julián David <julianfullstackdev@gmail.com>
2021-08-09 03:12:47 -0500

    default black bg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="es">
+      <Html lang="es" style={{ background: '#030308' }}>
         <Head>
           <link
             rel="preload"
