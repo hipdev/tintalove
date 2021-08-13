1979b09762d2b933a8bf8f239b4bf951195060a8
Julián David <julianfullstackdev@gmail.com>
2021-08-12 20:08:45 -0500

    fix

--- components/layout-steps/LayoutStepsArtist.tsx
+++ components/layout-steps/LayoutStepsArtist.tsx
@@ -8,7 +8,7 @@ import { UserState } from 'types/user'
 import SideMenuArtist from '../../components/layout-steps/SideMenuArtist'
 import { AiOutlineCamera } from 'react-icons/ai'
 import { VscMenu } from 'react-icons/vsc'
-import WrapperAvailability from '../../components/layout/header/WrapperAvailability'
+import WrapperAvailability from '../layout/Header/WrapperAvailability'
 import { Toaster } from 'react-hot-toast'
 
 type Props = {
