0f0d4c7548fad557bfd4c8f714aaf7dcd92dcb03
Julián David <julianfullstackdev@gmail.com>
2021-08-12 19:37:21 -0500

    small fix

--- components/isAuth.tsx
+++ components/isAuth.tsx
@@ -1,6 +1,6 @@
 import { signInWithPopup } from '@firebase/auth'
 import { auth } from 'lib/firebase'
-import { provider } from './layout/header/Submenu'
+import { provider } from 'components/layout/header/Submenu'
 import { createUser } from 'lib/queries/users'
 import useUserId from 'hooks/use-user-id'
 
