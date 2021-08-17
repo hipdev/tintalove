2829c506dcc7c8132bbf0271ce34ba0f33151ca9
Julián David <julianfullstackdev@gmail.com>
2021-08-17 15:56:05 -0500

    home posts now loading on client runtime

--- components/home/PostsListDesktop.tsx
+++ components/home/PostsListDesktop.tsx
@@ -1,7 +1,12 @@
+import Loading from 'components/loading/loading'
+import { getPostsInfo } from 'lib/queries/posts'
 import Masonry from 'react-masonry-css'
+import useSWR from 'swr'
 import PostItem from './PostItem'
 
-const PostsListDesktop = ({ posts, user }) => {
+const PostsListDesktop = ({ user }) => {
+  const { data } = useSWR(['getPostsInfo'], getPostsInfo)
+
   const breakpointColumnsObj = {
     default: 6,
     1600: 4,
@@ -9,6 +14,8 @@ const PostsListDesktop = ({ posts, user }) => {
     700: 2,
     500: 1,
   }
+
+  if (!data) return <Loading />
 
   return (
     <div className="px-5 sm:px-10 lg:px-20 pt-7 md:pt-24">
@@ -18,30 +25,36 @@ const PostsListDesktop = ({ posts, user }) => {
         columnClassName="my-masonry-grid_column"
       >
         {/* <div className="grid grid-cols-6 gap-6"> */}
-        {posts.length > 0 ? (
-          posts.map((post) => (
+        {data.posts.length > 0 ? (
+          data.posts.map((post) => (
             <PostItem key={Math.random()} post={post} user={user} />
           ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
-        {posts.length > 0 ? (
-          posts.map((post) => (
+        {data.posts.length > 0 ? (
+          data.posts.map((post) => (
             <PostItem key={Math.random()} post={post} user={user} />
           ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
-        {posts.length > 0 ? (
-          posts.map((post) => (
+        {data.posts.length > 0 ? (
+          data.posts.map((post) => (
             <PostItem key={Math.random()} post={post} user={user} />
           ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
         )}
-
-        {posts.length > 0 ? (
-          posts.map((post) => (
+        {data.posts.length > 0 ? (
+          data.posts.map((post) => (
+            <PostItem key={Math.random()} post={post} user={user} />
+          ))
+        ) : (
+          <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
+        )}
+        {data.posts.length > 0 ? (
+          data.posts.map((post) => (
             <PostItem key={Math.random()} post={post} user={user} />
           ))
         ) : (
--- components/home/PostsListMobile.tsx
+++ components/home/PostsListMobile.tsx
@@ -1,10 +1,17 @@
+import Loading from 'components/loading/loading'
+import { getPostsInfoMobile } from 'lib/queries/posts'
+import useSWR from 'swr'
 import PostItem from './PostItem'
 
-const PostsListMobile = ({ posts, user }) => {
+const PostsListMobile = ({ user }) => {
+  const { data } = useSWR(['getPostsInfo'], getPostsInfoMobile)
+
+  if (!data) return <Loading />
+
   return (
     <div className="px-5 sm:px-10 lg:px-20 pt-7 md:pt-24">
-      {posts.length > 0 ? (
-        posts.map((post) => (
+      {data.posts.length > 0 ? (
+        data.posts.map((post) => (
           <PostItem key={Math.random()} post={post} user={user} />
         ))
       ) : (
