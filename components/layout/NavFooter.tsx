cb6df5c24ff064730616565784566df7d6acf565
unknown <Julian David>
2021-08-16 22:35:59 -0500

    footer icons

--- components/layout/NavFooter.tsx
+++ components/layout/NavFooter.tsx
@@ -1,9 +1,8 @@
 import { useStateMachine } from 'little-state-machine'
 import Link from 'next/link'
-import { AiOutlineUnorderedList } from 'react-icons/ai'
-import { GoSearch } from 'react-icons/go'
-import { RiHome4Line } from 'react-icons/ri'
+import { AiOutlineHeart, AiOutlineUnorderedList } from 'react-icons/ai'
 import { lists } from 'lib/actions'
+import { BiHomeSmile, BiSearch } from 'react-icons/bi'
 
 const NavFooter = () => {
   const { _, actions }: any = useStateMachine({
@@ -12,24 +11,28 @@ const NavFooter = () => {
 
   return (
     <nav
-      className="flex bg-dark-800 fixed bottom-0 w-full justify-evenly text-gray-400  sm:hidden"
+      className="flex bg-dark-800 fixed bottom-0 w-full justify-evenly text-gray-300  sm:hidden"
       style={{ boxShadow: '0px -1px 4px #040404' }}
     >
       <Link href="/">
-        <a className="text-2xl p-3">
-          <RiHome4Line />
+        <a className="p-3 flex flex-col items-center">
+          <BiHomeSmile className="text-xl" />
+          <span className="text-xs">INICIO</span>
         </a>
       </Link>
       <Link href="/">
-        <a className="text-2xl p-3">
-          <GoSearch />
+      <a className="p-3 flex flex-col items-center">
+          <BiSearch className="text-xl" />
+          <span className="text-xs">BUSCAR</span>
         </a>
       </Link>
       <button
-        className="text-2xl p-3 focus:outline-none"
+        className=" p-3 focus:outline-none flex flex-col items-center" 
         onClick={() => actions.lists({ post: null, listOpen: true })}
       >
-        <AiOutlineUnorderedList />
+        <AiOutlineHeart className="text-xl" />
+        <span className="text-xs">FAVORITOS</span>
+
       </button>
     </nav>
   )
