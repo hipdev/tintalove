d8bce4591afd5741fbc529b8c2b08f19b150a094
Julián David <julianfullstackdev@gmail.com>
2021-08-16 23:23:56 -0500

    render posts based on view

--- components/home/PostsList.tsx
+++ components/home/PostsList.tsx
@@ -1,4 +1,5 @@
 import useUserId from 'hooks/use-user-id'
+import { useWindowSize } from 'hooks/useWindowSize'
 import { getUserInfo } from 'lib/queries/users'
 import { MdFilterList } from 'react-icons/md'
 import Masonry from 'react-masonry-css'
@@ -9,6 +10,8 @@ const PostListHome = ({ posts }) => {
   const { userId } = useUserId()
   const { data } = useSWR(userId ? userId : null, getUserInfo)
 
+  const size = useWindowSize()
+
   const breakpointColumnsObj = {
     default: 6,
     1600: 4,
@@ -17,15 +20,12 @@ const PostListHome = ({ posts }) => {
     500: 1,
   }
 
+  console.log(size, 'ancho')
+
   return (
     <div className="px-5 sm:px-10 lg:px-20 pt-7 md:pt-24">
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {/* <div className="grid grid-cols-6 gap-6"> */}
-        {posts.length > 0 ? (
+      {size.width <= 500 ? (
+        posts.length > 0 ? (
           posts.map((post) => (
             <PostItem
               key={Math.random()}
@@ -35,42 +35,61 @@ const PostListHome = ({ posts }) => {
           ))
         ) : (
           <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
-        )}
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <PostItem
-              key={Math.random()}
-              post={post}
-              user={data?.user || null}
-            />
-          ))
-        ) : (
-          <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
-        )}
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <PostItem
-              key={Math.random()}
-              post={post}
-              user={data?.user || null}
-            />
-          ))
-        ) : (
-          <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
-        )}
+        )
+      ) : (
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {/* <div className="grid grid-cols-6 gap-6"> */}
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <PostItem
+                key={Math.random()}
+                post={post}
+                user={data?.user || null}
+              />
+            ))
+          ) : (
+            <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
+          )}
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <PostItem
+                key={Math.random()}
+                post={post}
+                user={data?.user || null}
+              />
+            ))
+          ) : (
+            <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
+          )}
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <PostItem
+                key={Math.random()}
+                post={post}
+                user={data?.user || null}
+              />
+            ))
+          ) : (
+            <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
+          )}
 
-        {posts.length > 0 ? (
-          posts.map((post) => (
-            <PostItem
-              key={Math.random()}
-              post={post}
-              user={data?.user || null}
-            />
-          ))
-        ) : (
-          <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
-        )}
-      </Masonry>
+          {posts.length > 0 ? (
+            posts.map((post) => (
+              <PostItem
+                key={Math.random()}
+                post={post}
+                user={data?.user || null}
+              />
+            ))
+          ) : (
+            <p className="text-white bold text-2xl mb-10">Sin publicaciones</p>
+          )}
+        </Masonry>
+      )}
     </div>
   )
 }
