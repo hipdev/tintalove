23e1e8d24821573fb5ca01a5ef1366c6e77941fa
Julián David <julianfullstackdev@gmail.com>
2021-08-19 10:18:01 -0500

    advance more in the mobile version

--- components/layout/header/MenuMobile.tsx
+++ components/layout/header/MenuMobile.tsx
@@ -4,9 +4,13 @@ import { Dialog, Transition } from '@headlessui/react'
 import { IoMdClose } from 'react-icons/io'
 import { HiOutlineMenuAlt2 } from 'react-icons/hi'
 import Image from 'next/image'
+import { AiOutlineInstagram } from 'react-icons/ai'
+// import { UserState } from 'types/user'
 
-const MenuMobile = ({}: any) => {
+const MenuMobile = ({ user }: any) => {
   const [open, setOpen] = useState(false)
+
+  console.log(user, 'usuario')
 
   return (
     <>
@@ -30,15 +34,15 @@ const MenuMobile = ({}: any) => {
             <div className="fixed inset-y-0 right-0 max-w-full flex">
               <Transition.Child
                 as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enter="transform transition ease-in-out duration-300 sm:duration-700"
                 enterFrom="translate-x-full"
                 enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leave="transform transition ease-in-out duration-300 sm:duration-700"
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
                 <div className="w-screen max-w-md">
-                  <div className="h-full divide-y divide-gray-200 flex flex-col bg-dark-800 shadow-xl">
+                  <div className="h-full flex flex-col bg-gr-800 shadow-xl">
                     <div className="min-h-0 flex-1 flex flex-col py-6 overflow-y-scroll">
                       <div className="px-4 sm:px-6">
                         <div className="flex items-start justify-between">
@@ -55,7 +59,7 @@ const MenuMobile = ({}: any) => {
                           <div className="ml-3 h-7 flex items-center">
                             <button
                               type="button"
-                              className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                              className="rounded-md text-gray-300 hover:text-gray-500 "
                               onClick={() => setOpen(false)}
                             >
                               <span className="sr-only">Close panel</span>
@@ -69,27 +73,24 @@ const MenuMobile = ({}: any) => {
                       </div>
                       <div className="mt-6 relative flex-1 px-4 sm:px-6">
                         {/* Replace with your content */}
-                        <div
-                          className="h-full border-2 border-dashed border-gray-200"
-                          aria-hidden="true"
-                        />
+                        <div></div>
                         {/* /End replace */}
                       </div>
                     </div>
-                    <div className="flex-shrink-0 px-4 py-4 flex justify-center">
-                      <button
-                        type="button"
-                        className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        onClick={() => setOpen(false)}
-                      >
-                        Cancel
+                    <div className="flex-shrink-0 px-4 py-4 flex justify-between items-center bg-gr-900">
+                      <button className="bg-primary text-gray-300 px-7 py-3 font-semibold uppercase rounded-sm hover:bg-primaryHover">
+                        Ingresar
                       </button>
-                      <button
-                        type="submit"
-                        className="ml-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        Save
-                      </button>
+                      <div>
+                        <a
+                          className="text-white text-4xl "
+                          href="https://www.instagram.com/tinta.love/"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <AiOutlineInstagram />
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
