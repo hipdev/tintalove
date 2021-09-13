847943c3c8c7d11f8560ca13c37f2108ee35e3c6
Julián David <julianfullstackdev@gmail.com>
2021-09-12 22:30:31 -0500

    prepare for step 3 with supa

--- pages/studio-account/contact-info.tsx
+++ pages/studio-account/contact-info.tsx
@@ -4,8 +4,8 @@ import { Loader } from '@googlemaps/js-api-loader'
 import IsAuth from 'components/isAuth'
 import LayoutStepsStudio from 'components/layout-steps/LayoutStepsStudio'
 import ContactInfoStudio from 'components/studio-account/contact-info/ContactInfo'
-import { getArtistInfo } from 'lib/queries/artists'
 import { useUser } from 'hooks/useUser'
+import { getStudioData } from 'lib/queries/studios'
 
 const loader = new Loader({
   apiKey: 'AIzaSyA5drETj_sJmO1kGEDEb7tXWzwJb05ipCY', // api key de google maps
@@ -15,10 +15,11 @@ const loader = new Loader({
 export default function ContactInfoPageStudio() {
   const { user }: any = useUser()
 
-  const { data: artist } = useSWR(
-    user?.id ? ['getArtistFullInfo', user.id] : null,
-    getArtistInfo
+  const { data: dataStudio } = useSWR(
+    user?.id ? ['getStudioData', user.id] : null,
+    getStudioData
   )
+
   const [loadMap, setLoadMap] = useState(false)
 
   loader
@@ -30,13 +31,18 @@ export default function ContactInfoPageStudio() {
       console.log('error loading Google Maps API')
     })
 
-  if (!user && !artist) {
+  if (!user && !dataStudio) {
     return <IsAuth>Cargando data...</IsAuth>
   }
 
   return (
     <LayoutStepsStudio uid={user.id} user={user}>
-      {loadMap && <ContactInfoStudio hasStudio={user} studioId={user.id} />}
+      {loadMap && (
+        <ContactInfoStudio
+          studioData={dataStudio?.studios}
+          studioId={dataStudio?.studio_id}
+        />
+      )}
     </LayoutStepsStudio>
   )
 }
