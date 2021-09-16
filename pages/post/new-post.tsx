b000da5a1254a9f8013e28863e796b6990431cc2
Julian David <julianfullstackdev@gmail.com>
2021-09-16 13:24:18 -0500

    small changes for post

--- pages/post/new-post.tsx
+++ pages/post/new-post.tsx
@@ -2,19 +2,30 @@ import Layout from 'components/layout/Layout'
 import Loading from 'components/loading/loading'
 import CreatePost from 'components/post/create/CreatePost'
 import useUserId from 'hooks/use-user-id'
+import { useUser } from 'hooks/useUser'
+import { getArtistFullInfo } from 'lib/queries/artists'
 import { getUserInfo } from 'lib/queries/users'
+import { useState } from 'react'
 import useSWR from 'swr'
 
 export default function PostCreatePage() {
-  const { userId } = useUserId()
-  const { data } = useSWR(userId ? userId : null, getUserInfo)
+  const { user }: any = useUser()
 
-  if (!data) return <Loading />
+  console.log(user, 'el usuario')
+
+  const [loadMap, setLoadMap] = useState(false)
+
+  const { data: artist } = useSWR(
+    user?.id ? ['getArtistFullInfo', user.id] : null,
+    getArtistFullInfo
+  )
+
+  if (!user && !artist) return <Loading />
 
   return (
     <Layout>
-      {data?.user.artist_active ? (
-        <CreatePost user={data?.user} />
+      {artist?.is_active ? (
+        <CreatePost user={user} />
       ) : (
         <div className="h-screen flex justify-center bg-gray-800">
           <p className="text-3xl text-gray-300 mt-20">
